0ea944010fe93740601c0817de5b69b79e813730
Tim Rupp <caphrim007@gmail.com>
2016-04-21 15:11:16 -0700

    corrected license and formatting

--- library/bigip_dns_record.py
+++ library/bigip_dns_record.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
-
-# Ansible module to manage BIG-IP devices
+# -*- coding: utf-8 -*-
 #
-# This module covers the ResourceRecord interfaces described in the iControl
-# Management documentation.
+# This file is part of Ansible
 #
-# More information can be found here
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    https://devcentral.f5.com/wiki/iControl.Management.ashx
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 
 DOCUMENTATION = '''
 ---
@@ -43,7 +49,8 @@ notes:
      pip install bigsuds
 
 requirements: [ "bigsuds", "distutils" ]
-author: Tim Rupp <t.rupp@f5.com>
+author:
+    - Tim Rupp <caphrim007@gmail.com> (@caphrim007)
 '''
 
 EXAMPLES = '''
