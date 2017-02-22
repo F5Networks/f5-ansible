97a35dd8aba14bae6a8ff6232651471766531963
Tim Rupp <caphrim007@gmail.com>
2017-02-21 16:39:55 -0800

    refactored bigip provision to be reasonably sane and complete

--- library/bigip_provision.py
+++ library/bigip_provision.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2016 F5 Networks Inc.
+# Copyright 2017 F5 Networks Inc.
 #
 # This file is part of Ansible
 #
@@ -24,9 +24,7 @@ module: bigip_provision
 short_description: Manage BIG-IP module provisioning
 description:
   - Manage BIG-IP module provisioning. This module will only provision at the
-    standard levels of Dedicated, Nominal, and Minimum. While iControl SOAP
-    additionally supports a Custom level, this level is not supported by this
-    module.
+    standard levels of Dedicated, Nominal, and Minimum.
 version_added: "2.3"
 options:
   module:
@@ -64,7 +62,7 @@ options:
         guarantees that the specified module is provisioned at the requested
         level provided that there are sufficient resources on the device (such
         as physical RAM) to support the provisioned module. When C(absent),
-        unprovisions the module.
+        deprovision the module.
     required: false
     default: present
     choices:
@@ -73,8 +71,10 @@ options:
 notes:
   - Requires the f5-sdk Python package on the host. This is as easy as pip
     install f5-sdk.
+  - This module only works reliably on BIG-IP versions >= 13.1.
+  - After you provision something you should
 requirements:
-  - f5-sdk
+  - f5-sdk >= 2.2.3
 extends_documentation_fragment: f5
 author:
   - Tim Rupp (@caphrim007)
@@ -102,132 +102,353 @@ EXAMPLES = '''
   delegate_to: localhost
 '''
 
-import socket
+
+import time
 
 try:
-    from f5.bigip import ManagementRoot
-    from icontrol.session import iControlUnexpectedHTTPError
-    HAS_F5SDK = True
+    from ansible.module_utils.f5_utils import *
 except ImportError:
-    HAS_F5SDK = False
+    # This is a temporary placeholder for folks that dont yet have
+    # Ansible 2.4
+    try:
+        from f5.bigip import ManagementRoot as BigIpMgmt
+        from f5.bigip.contexts import TransactionContextManager as BigIpTxContext
+        from f5.bigiq import ManagementRoot as BigIqMgmt
+        from f5.iworkflow import ManagementRoot as iWorkflowMgmt
+        from icontrol.session import iControlUnexpectedHTTPError
+        HAS_F5SDK = True
+    except ImportError:
+        HAS_F5SDK = False
 
-class BigIpCommon(object):
-    def __init__(self, module):
-        self._username = module.params.get('user')
-        self._password = module.params.get('password')
-        self._hostname = module.params.get('server')
-
-        self._level = module.params.get('level')
-        self._module = module.params.get('module')
-        self._validate_certs = module.params.get('validate_certs')
-
-        # Check if we can connect to the device
-        sock = socket.create_connection((self._hostname,443), 60)
-        sock.close()
-
-class BigIpRest(BigIpCommon):
-    def __init__(self, module):
-        super(BigIpRest, self).__init__(module)
-
-        self._uri = 'https://%s/mgmt/tm/sys/db/%s' % (self._hostname, self._key)
-        self._headers = {
-            'Content-Type': 'application/json'
-        }
-
-    def read(self):
-        resp = requests.get(self._uri,
-                            auth=(self._username, self._password),
-                            verify=self._validate_certs)
-
-        if resp.status_code != 200:
-            return {}
-        else:
-            return resp.json()
-
-    def present(self):
-        changed = False
-        current = self.read()
-
-        if current and current['name'] == self._key:
-            if current['value'] != self._value:
-                resp = requests.put(self._uri,
-                                    auth=(self._username, self._password),
-                                    data=json.dumps(self._payload),
-                                    verify=self._validate_certs)
-                if resp.status_code == 200:
-                    changed = True
-                else:
-                    raise Exception('Failed to set the provided key')
-
-        return changed
+    from ansible.module_utils.basic import env_fallback
+    from ansible.module_utils.basic import *
+    from ansible.module_utils.six import iteritems
 
 
-def main():
-    changed = False
-
-    module_choices = [
-        'afm', 'am', 'sam', 'asm', 'avr', 'fps',
-        'gtm', 'lc', 'ltm', 'pem', 'swg'
-    ]
-
-    argument_spec = f5_argument_spec()
-
-    meta_args = dict(
-        server=dict(required=True),
-        module=dict(required=True, choices=module_choices),
-        level=dict(default='nominal', choices=['nominal', 'dedicated', 'minimal']),
-        password=dict(default='admin'),
-        state=dict(default='present', choices=['present', 'reset']),
-        user=dict(default='admin'),
+    F5_COMMON_ARGS = dict(
+        server=dict(
+            type='str',
+            required=True,
+            fallback=(env_fallback, ['F5_SERVER'])
+        ),
+        user=dict(
+            type='str',
+            required=True,
+            fallback=(env_fallback, ['F5_USER'])
+        ),
+        password=dict(
+            type='str',
+            aliases=['pass', 'pwd'],
+            required=True,
+            no_log=True,
+            fallback=(env_fallback, ['F5_PASSWORD'])
+        ),
         validate_certs=dict(
             default='yes',
             type='bool',
-            choices=BOOLEANS
+            fallback=(env_fallback, ['F5_VALIDATE_CERTS'])
+        ),
+        server_port=dict(
+            type='int',
+            default=443,
+            required=False,
+            fallback=(env_fallback, ['F5_SERVER_PORT'])
+        ),
+        state=dict(
+            type='str',
+            default='present',
+            choices=['present', 'absent']
+        ),
+        partition=dict(
+            type='str',
+            default='Common',
+            fallback=(env_fallback, ['F5_PARTITION'])
         )
     )
-    argument_spec.update(meta_args)
 
-    module = AnsibleModule(
-        argument_spec=argument_spec,
-        supports_check_mode=False
+
+    class AnsibleF5Client(object):
+        def __init__(self, argument_spec=None, supports_check_mode=False,
+                     mutually_exclusive=None, required_together=None,
+                     required_if=None, required_one_of=None,
+                     f5_product_name='bigip'):
+
+            merged_arg_spec = dict()
+            merged_arg_spec.update(F5_COMMON_ARGS)
+            if argument_spec:
+                merged_arg_spec.update(argument_spec)
+                self.arg_spec = merged_arg_spec
+
+            mutually_exclusive_params = []
+            if mutually_exclusive:
+                mutually_exclusive_params += mutually_exclusive
+
+            required_together_params = []
+            if required_together:
+                required_together_params += required_together
+
+            self.module = AnsibleModule(
+                argument_spec=merged_arg_spec,
+                supports_check_mode=supports_check_mode,
+                mutually_exclusive=mutually_exclusive_params,
+                required_together=required_together_params,
+                required_if=required_if,
+                required_one_of=required_one_of
+            )
+
+            self.check_mode = self.module.check_mode
+            self._connect_params = self._get_connect_params()
+
+            try:
+                self.api = self._get_mgmt_root(
+                    f5_product_name, **self._connect_params
+                )
+            except iControlUnexpectedHTTPError as exc:
+                self.fail(str(exc))
+
+        def fail(self, msg):
+            self.module.fail_json(msg=msg)
+
+        def _get_connect_params(self):
+            params = dict(
+                user=self.module.params['user'],
+                password=self.module.params['password'],
+                server=self.module.params['server'],
+                server_port=self.module.params['server_port'],
+                validate_certs=self.module.params['validate_certs']
+            )
+            return params
+
+        def _get_mgmt_root(self, type, **kwargs):
+            if type == 'bigip':
+                return BigIpMgmt(
+                    kwargs['server'],
+                    kwargs['user'],
+                    kwargs['password'],
+                    port=kwargs['server_port'],
+                    token='tmos'
+                )
+            elif type == 'iworkflow':
+                return iWorkflowMgmt(
+                    kwargs['server'],
+                    kwargs['user'],
+                    kwargs['password'],
+                    port=kwargs['server_port'],
+                    token='local'
+                )
+            elif type == 'bigiq':
+                return BigIqMgmt(
+                    kwargs['server'],
+                    kwargs['user'],
+                    kwargs['password'],
+                    port=kwargs['server_port'],
+                    token='local'
+                )
+
+
+    class AnsibleF5Parameters(object):
+        def __init__(self, params=None):
+            self._partition = None
+            if params is None:
+                return
+            for key, value in iteritems(params):
+                setattr(self, key, value)
+
+        @property
+        def partition(self):
+            if self._partition is None:
+                return 'Common'
+            return self._partition.strip('/')
+
+        @partition.setter
+        def partition(self, value):
+            self._partition = value
+
+        @classmethod
+        def from_api(cls, params):
+            for key,value in iteritems(cls.param_api_map):
+                params[key] = params.pop(value, None)
+            p = cls(params)
+            return p
+
+        def __getattr__(self, item):
+            return None
+
+        def api_params(self):
+            result = self._api_params_from_map()
+            return self._filter_none(result)
+
+        def _filter_none(self, params):
+            result = dict()
+            for k, v in iteritems(params):
+                if v is None:
+                    continue
+                result[k] = v
+            return result
+
+        def _api_params_from_map(self):
+            result = dict()
+            pmap = self.__class__.param_api_map
+            for k,v in iteritems(pmap):
+                value = getattr(self, k)
+                result[v] = value
+            return result
+
+
+class Parameters(AnsibleF5Parameters):
+    param_api_map = dict(
+        level='level'
     )
 
-    hostname = module.params.get('server')
-    password = module.params.get('password')
-    username = module.params.get('user')
-    state = module.params.get('state')
+class ModuleManager(object):
+    def __init__(self, client):
+        self.client = client
+        self.have = None
+        self.want = Parameters(self.client.module.params)
+        self.changes = Parameters()
 
-    try:
-        if connection == 'icontrol':
-            if not bigsuds_found:
-                raise Exception("The python bigsuds module is required")
+    def exec_module(self):
+        if not HAS_F5SDK:
+            raise F5ModuleError("The python f5-sdk module is required")
 
-            icontrol = test_icontrol(username, password, hostname)
-            if icontrol:
-                obj = BigIpIControl(module)
-        elif connection == 'rest':
-            if not requests_found:
-                raise Exception("The python requests module is required")
+        changed = False
+        result = dict()
+        state = self.want.state
 
-            obj = BigIpRest(module)
+        try:
+            if state == "present":
+                changed = self.update()
+            elif state == "absent":
+                changed = self.absent()
+        except iControlUnexpectedHTTPError as e:
+            raise F5ModuleError(str(e))
 
-        if state == "present":
-            if obj.present():
-                changed = True
-        elif state == "absent":
-            if obj.absent():
-                changed = True
-    except bigsuds.ConnectionError:
-        module.fail_json(msg="Could not connect to BIG-IP host %s" % hostname)
-    except socket.timeout:
-        module.fail_json(msg="Timed out connecting to the BIG-IP")
-    except bigsuds.ConnectionError as e:
-        module.fail_json(msg=str(e))
+        result.update(vars(self.changes))
+        result.update(dict(changed=changed))
+        return result
 
-    module.exit_json(changed=changed)
+    def exists(self):
+        provision = self.client.api.tm.sys.provision
+        resource = getattr(provision, self.want.module)
+        resource = resource.load()
+        result = resource.to_dict()
+        result.pop('_meta_data', None)
+        if str(result.level) == 'none':
+            return False
+        return True
 
-from ansible.module_utils.basic import *
-from ansible.module_utils.f5 import *
+    def update(self):
+        self.have = self.read_current_from_device()
+        if not self.should_update():
+            return False
+        if self.client.check_mode:
+            return True
+        self.update_on_device()
+        self.wait_for_module_provisioning()
+        return True
+
+    def should_update(self):
+        if self.want.level != self.have.level:
+            return True
+        return False
+
+    def update_on_device(self):
+        params = self.want.api_params()
+        provision = self.client.api.tm.sys.provision
+        resource = getattr(provision, self.want.module)
+        resource = resource.load()
+        resource.update(**params)
+
+    def read_current_from_device(self):
+        provision = self.client.api.tm.sys.provision
+        resource = getattr(provision, self.want.module)
+        result = resource.to_dict()
+        result.pop('_meta_data', None)
+        return Parameters.from_api(result)
+
+    def absent(self, ):
+        if self.exists():
+            return self.remove()
+        return False
+
+    def remove(self):
+        if self.client.check_mode:
+            return True
+        self.remove_from_device()
+        self.wait_for_module_provisioning()
+        if self.exists():
+            raise F5ModuleError("Failed to deprovision the module")
+        return True
+
+    def remove_from_device():
+        provision = self.client.api.tm.sys.provision
+        resource = getattr(provision, self.want.module)
+        resource = resource.load()
+        resource.update({'level': self.want.level})
+
+    def wait_for_module_provisioning(self):
+        # To prevent things from running forever, the hack is to check
+        # for mprov's status twice. If mprov is finished, then in most
+        # cases (not ASM) the provisioning is probably ready.
+        nops = 0
+
+        # Sleep a little to let provisioning settle and begin properly
+        time.sleep(5)
+
+        while nops < 2:
+            if not self._is_mprov_running_on_device():
+                nops += 1
+            time.sleep(5)
+
+    def _is_mprov_running_on_device(self):
+        output = self.client.api.tm.util.bash.exec_cmd(
+            'run',
+            utilCmdArgs='-c "ps aux | grep \'[m]prov\'"'
+        )
+        if hasattr(output, 'commandResult'):
+            return True
+        return False
+
+
+class ArgumentSpec(object):
+    def __init__(self):
+        self.supports_check_mode = True
+        self.argument_spec = dict(
+            module=dict(
+                required=True,
+                choices=[
+                    'afm', 'am', 'sam', 'asm', 'avr', 'fps',
+                    'gtm', 'lc', 'ltm', 'pem', 'swg'
+                ]
+            ),
+            level=dict(
+                default='nominal',
+                choices=['nominal', 'dedicated', 'minimal']
+            ),
+            state=dict(
+                default='present',
+                choices=['present', 'absent']
+            )
+        )
+        self.mutually_exclusive = [
+            ['parameters', 'parameters_src']
+        ]
+        self.f5_product_name = 'bigip'
+
+
+def main():
+    spec = ArgumentSpec()
+
+    client = AnsibleF5Client(
+        argument_spec=spec.argument_spec,
+        mutually_exclusive=spec.mutually_exclusive,
+        supports_check_mode=spec.supports_check_mode,
+        f5_product_name=spec.f5_product_name
+    )
+
+    mm = ModuleManager(client)
+    results = mm.exec_module()
+    client.module.exit_json(**results)
 
 if __name__ == '__main__':
     main()
---
+++ test/bigip/test_bigip_provision.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 F5 Networks Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import json
+
+from ansible.compat.tests import unittest
+from ansible.compat.tests.mock import patch, Mock
+from ansible.module_utils import basic
+from ansible.module_utils._text import to_bytes
+from ansible.module_utils.f5_utils import *
+from library.bigip_provision import (
+    Parameters,
+    ModuleManager,
+    ArgumentSpec
+)
+
+
+fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
+fixture_data = {}
+
+
+def set_module_args(args):
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
+    basic._ANSIBLE_ARGS = to_bytes(args)
+
+
+def load_fixture(name):
+    path = os.path.join(fixture_path, name)
+
+    if path in fixture_data:
+        return fixture_data[path]
+
+    with open(path) as f:
+        data = f.read()
+
+    try:
+        data = json.loads(data)
+    except:
+        pass
+
+    fixture_data[path] = data
+    return data
+
+
+class TestParameters(unittest.TestCase):
+    def test_module_parameters(self):
+        args = dict(
+            module='gtm',
+            password='password',
+            server='localhost',
+            user='admin'
+        )
+        p = Parameters(args)
+        assert p.module == 'gtm'
+
+
+class TestManager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
+    @patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
+           return_value=True)
+    def test_provision_one_module_default_level(self, *args):
+        # Configure the arguments that would be sent to the Ansible module
+        set_module_args(dict(
+            module='gtm',
+            password='passsword',
+            server='localhost',
+            user='admin'
+        ))
+
+        # Configure the parameters that would be returned by querying the
+        # remote device
+        current = Parameters(
+            dict(
+                module='gtm',
+                level='none'
+            )
+        )
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+        mm = ModuleManager(client)
+
+        # Override methods to force specific logic in the module to happen
+        mm.exit_json = lambda x: False
+        mm.update_on_device = lambda: True
+        mm.read_current_from_device = lambda: current
+
+        # this forced sleeping can cause these tests to take 15
+        # or more seconds to run. This is deliberate.
+        mm._is_mprov_running_on_device = Mock()
+        mm._is_mprov_running_on_device.side_effect = [True, False, False]
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
