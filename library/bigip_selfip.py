028436649e6b3d1d236cbcfd8a26a3d68a051950
Tim Rupp <caphrim007@gmail.com>
2017-03-20 11:55:26 -0700

    corrected docs

--- library/bigip_selfip.py
+++ library/bigip_selfip.py
@@ -36,27 +36,31 @@ options:
     description:
       - The IP addresses for the new self IP. This value is ignored upon update
         as addresses themselves cannot be changed after they are created.
+    required: False
+    default: None
   allow_service:
     description:
       - Configure port lockdown for the Self IP. By default, the Self IP has a
         "default deny" policy. This can be changed to allow TCP and UDP ports
         as well as specific protocols. This list should contain C(protocol):C(port)
         values.
+    required: False
+    default None
   name:
     description:
       - The self IP to create.
-    required: true
+    required: True
     default: Value of C(address)
   netmask:
     description:
       - The netmasks for the self IP.
-    required: true
+    required: True
   state:
     description:
       - The state of the variable on the system. When C(present), guarantees
         that the Self-IP exists with the provided attributes. When C(absent),
         removes the Self-IP from the system.
-    required: false
+    required: False
     default: present
     choices:
       - absent
@@ -65,16 +69,16 @@ options:
     description:
       - The traffic group for the self IP addresses in an active-active,
         redundant load balancer configuration.
-    required: false
+    required: False
   vlan:
     description:
       - The VLAN that the new self IPs will be on.
-    required: true
+    required: True
   route_domain:
     description:
         - The route domain id of the system.
           If none, id of the route domain will be "0" (default route domain)
-    required: false
+    required: False
     default: none
     version_added: 2.3
 notes:
