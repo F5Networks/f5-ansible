746fb3fa9486d7e7d1352f07333215a586bfd96d
Tim Rupp <caphrim007@gmail.com>
2017-03-20 11:55:07 -0700

    added functional tests for iworkflow system setup

--- test/iworkflow/test_iworkflow_system_setup.py
+++ test/iworkflow/test_iworkflow_system_setup.py
@@ -25,10 +25,17 @@ import os
 import json
 
 from ansible.compat.tests import unittest
+from ansible.compat.tests.mock import patch
 from ansible.module_utils import basic
 from ansible.module_utils._text import to_bytes
+from ansible.module_utils.f5_utils import (
+    AnsibleF5Client,
+    F5ModuleError
+)
 from library.iworkflow_system_setup import (
-    Parameters,
+    ArgumentSpec,
+    ModuleManager,
+    Parameters
 )
 
 
@@ -91,3 +98,51 @@ class TestParameters(unittest.TestCase):
 
         p = Parameters(args)
         assert p.hostname == 'iworkflow1.local'
+
+
+@patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
+       return_value=True)
+class TestManager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
+    def test_system_setup(self, *args):
+        set_module_args(dict(
+            hostname='iworkflow1.local',
+            discovery_address='10.2.2.3',
+            management_address='10.0.2.15/24',
+            dns_servers=['10.0.2.3'],
+            ntp_servers=['pool.ntp.org'],
+            server='localhost',
+            user='admin',
+            password='password'
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        mm = ModuleManager(client)
+
+        easy_setup = load_fixture('current_easy_setup.json')
+        setup = load_fixture('current_setup.json')
+        discovered = load_fixture('current_identified_devices_config_discovery.json')
+
+        args = dict()
+        args.update(easy_setup)
+        args.update(setup),
+        args.update(discovered)
+        current = Parameters(args)
+
+        # Override methods to force specific logic in the module to happen
+        mm.exit_json = lambda x: True
+        mm.read_current_from_device = lambda: current
+        mm.exists = lambda: False
+        mm.update_on_device = lambda: True
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
