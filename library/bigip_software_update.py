e0ff2bba20e642d0ddf5fcbbc34287272115b393
Tim Rupp <caphrim007@gmail.com>
2017-04-20 11:04:46 -0700

    refactor the software_update module

--- library/bigip_software_update.py
+++ library/bigip_software_update.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2016 F5 Networks Inc.
+# Copyright 2017 F5 Networks Inc.
 #
 # This file is part of Ansible
 #
@@ -27,56 +27,35 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = '''
 ---
 module: bigip_software_update
-short_description: Manage the software update settings of a BIG-IP
+short_description: Manage the software update settings of a BIG-IP.
 description:
-  - Manage the software update settings of a BIG-IP
-version_added: "2.2"
+  - Manage the software update settings of a BIG-IP.
+version_added: "2.4"
 options:
   auto_check:
     description:
       - Specifies whether to automatically check for updates on the F5
-        Networks downloads server
-    required: false
+        Networks downloads server.
+    required: False
     default: None
+    choices:
+      - yes
+      - no
   frequency:
     description:
-      - Specifies the schedule for the automatic update check
-    required: false
+      - Specifies the schedule for the automatic update check.
+    required: False
     default: None
     choices:
       - daily
       - monthly
       - weekly
-  password:
-    description:
-      - BIG-IP password
-    required: true
-  server:
-    description:
-      - BIG-IP host
-    required: true
-  server_port:
-    description:
-      - BIG-IP server port
-    required: false
-    default: 443
-  user:
-    description:
-      - BIG-IP username
-    required: true
-    aliases:
-      - username
-  validate_certs:
-    description:
-      - If C(no), SSL certificates will not be validated. This should only be
-        used on personally controlled sites using self-signed certificates.
-    required: false
-    default: true
 notes:
   - Requires the f5-sdk Python package on the host This is as easy as pip
     install f5-sdk
+extends_documentation_fragment: f5
 requirements:
-  - f5-sdk
+  - f5-sdk >= 2.2.3
 author:
   - Tim Rupp (@caphrim007)
 '''
@@ -87,90 +66,144 @@ EXAMPLES = '''
 RETURN = '''
 '''
 
-try:
-    from f5.bigip import ManagementRoot
-    HAS_F5SDK = True
-except ImportError:
-    HAS_F5SDK = False
+from ansible.module_utils.basic import BOOLEANS
+from ansible.module_utils.f5_utils import (
+    AnsibleF5Client,
+    AnsibleF5Parameters,
+    HAS_F5SDK,
+    F5ModuleError,
+    iControlUnexpectedHTTPError
+)
 
 
-AUTOCHECK = ['disabled', 'enabled']
-FREQ = ['daily', 'monthly', 'weekly']
+class Parameters(AnsibleF5Parameters):
+    api_map = {
+        'autoCheck': 'auto_check'
+    }
 
+    updatables = [
+        'auto_check', 'frequency'
+    ]
 
-class BigIpSoftwareUpdate(object):
-    def __init__(self, *args, **kwargs):
-        if not HAS_F5SDK:
-            raise F5ModuleError("The python f5-sdk module is required")
+    returnables = [
+        'auto_check', 'frequency'
+    ]
 
-        self.params = kwargs
-        self.api = ManagementRoot(kwargs['server'],
-                                  kwargs['user'],
-                                  kwargs['password'],
-                                  port=kwargs['server_port'])
-
-    def update(self):
-        changed = False
-        current = self.read()
-
-        if self.params['auto_check']:
-            if self.params['auto_check'] != current['autoCheck']:
-                changed = True
+    @property
+    def auto_check(self):
+        if self._values['auto_check'] is None:
+            return None
+        elif self._values['auto_check'] in [True, 'enabled']:
+            return 'enabled'
         else:
-            del self.params['auto_check']
+            return 'disabled'
 
-        if self.params['frequency']:
-            if self.params['frequency'] != current['frequency']:
-                changed = True
-        else:
-            del self.params['frequency']
-
-        if self.params['check_mode']:
-            return changed
-
-        r = self.api.tm.sys.software.update.load()
-        r.update(**self.params)
-
-        return True
-
-    def read(self):
-        r = self.api.tm.sys.software.update.load()
-        return r
-
-    def flush(self):
-        changed = self.update()
-        current = self.read()
-        result.update(**current)
-
-        result.update(dict(changed=changed))
+    def api_params(self):
+        result = {}
+        for api_attribute in self.api_attributes:
+            if self.network == 'default':
+                result['network'] = None
+            elif self.api_map is not None and api_attribute in self.api_map:
+                result[api_attribute] = getattr(self, self.api_map[api_attribute])
+            else:
+                result[api_attribute] = getattr(self, api_attribute)
+        result = self._filter_params(result)
         return result
 
 
+class ModuleManager(object):
+    def __init__(self, client):
+        self.client = client
+        self.have = None
+        self.want = Parameters(self.client.module.params)
+        self.changes = Parameters()
+
+    def exec_module(self):
+        result = dict()
+
+        try:
+            changed = self.update()
+        except iControlUnexpectedHTTPError as e:
+            raise F5ModuleError(str(e))
+
+        changes = self.changes.to_return()
+        result.update(**changes)
+        result.update(dict(changed=changed))
+        return result
+
+    def _update_changed_options(self):
+        changed = {}
+        for key in Parameters.updatables:
+            if getattr(self.want, key) is not None:
+                attr1 = getattr(self.want, key)
+                attr2 = getattr(self.have, key)
+                if attr1 != attr2:
+                    changed[key] = attr1
+        if changed:
+            self.changes = Parameters(changed)
+            return True
+        return False
+
+    def should_update(self):
+        result = self._update_changed_options()
+        if result:
+            return True
+        return False
+
+    def update(self):
+        self.have = self.read_current_from_device()
+        if not self.should_update():
+            return False
+        if self.client.check_mode:
+            return True
+        self.update_on_device()
+        return True
+
+    def update_on_device(self):
+        params = self.want.api_params()
+        result = self.client.api.tm.sys.software.update.load()
+        result.modify(**params)
+
+    def read_current_from_device(self):
+        resource = self.client.api.tm.sys.software.update.load()
+        result = resource.attrs
+        return Parameters(result)
+
+
+class ArgumentSpec(object):
+    def __init__(self):
+        self.supports_check_mode = True
+        self.argument_spec = dict(
+            auto_check=dict(
+                required=False,
+                default=None,
+                choices=BOOLEANS,
+                type='bool'
+            ),
+            frequency=dict(
+                required=False,
+                default=None,
+                choices=['daily', 'monthly', 'weekly']
+            )
+        )
+        self.f5_product_name = 'bigip'
+
+
 def main():
-    argument_spec = f5_argument_spec()
+    if not HAS_F5SDK:
+        raise F5ModuleError("The python f5-sdk module is required")
 
-    meta_args = dict(
-        auto_check=dict(required=False, default=None, choices=AUTOCHECK),
-        frequency=dict(required=False, default=None, choices=FREQ),
-        state=dict(default='present', choices=['present'])
-    )
-    argument_spec.update(meta_args)
+    spec = ArgumentSpec()
 
-    module = AnsibleModule(
-        argument_spec=argument_spec,
-        supports_check_mode=True
+    client = AnsibleF5Client(
+        argument_spec=spec.argument_spec,
+        supports_check_mode=spec.supports_check_mode,
+        f5_product_name=spec.f5_product_name
     )
 
-    try:
-        obj = BigIpSoftwareUpdate(**module.params)
-        result = obj.flush()
-
-        module.exit_json(**result)
-    except F5ModuleError as e:
-        module.fail_json(msg=str(e))
-
-from ansible.module_utils.basic import *
-from ansible.module_utils.f5_utils import *
+    mm = ModuleManager(client)
+    results = mm.exec_module()
+    client.module.exit_json(**results)
 
 if __name__ == '__main__':
     main()
---
+++ test/unit/bigip/test_bigip_software_update.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 F5 Networks Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import json
+
+from ansible.compat.tests import unittest
+from ansible.compat.tests.mock import patch
+from ansible.module_utils import basic
+from ansible.module_utils._text import to_bytes
+from ansible.module_utils.f5_utils import (
+    AnsibleF5Client
+)
+from library.bigip_software_update import (
+    Parameters,
+    ModuleManager,
+    ArgumentSpec
+)
+
+
+fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
+fixture_data = {}
+
+
+def set_module_args(args):
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
+    basic._ANSIBLE_ARGS = to_bytes(args)
+
+
+def load_fixture(name):
+    path = os.path.join(fixture_path, name)
+
+    if path in fixture_data:
+        return fixture_data[path]
+
+    with open(path) as f:
+        data = f.read()
+
+    try:
+        data = json.loads(data)
+    except:
+        pass
+
+    fixture_data[path] = data
+    return data
+
+
+class TestParameters(unittest.TestCase):
+    def test_module_parameters(self):
+        args = dict(
+            auto_check=True,
+            frequency="daily"
+        )
+        p = Parameters(args)
+        assert p.auto_check == 'enabled'
+        assert p.frequency == 'daily'
+
+    def test_api_parameters(self):
+        args = dict(
+            autoCheck="enabled",
+            frequency="daily"
+        )
+        p = Parameters(args)
+        assert p.auto_check == 'enabled'
+        assert p.frequency == 'daily'
