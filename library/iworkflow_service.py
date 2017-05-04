e935256522bdec76516198f2ff80dc3529bc9f62
Tim Rupp <caphrim007@gmail.com>
2017-05-04 13:41:59 -0700

    updates from downstream

--- library/iworkflow_service.py
+++ library/iworkflow_service.py
@@ -89,7 +89,6 @@ RETURN = '''
 
 '''
 
-
 from ansible.module_utils.f5_utils import *
 
 
@@ -210,11 +209,11 @@ class Parameters(AnsibleF5Parameters):
                     "One of the provided tables does not have a name"
                 )
             tmp['name'] = str(name)
-            columns = table.get('columnNames', None)
+            columns = table.get('columns', None)
             if columns:
-                tmp['columnNames'] = []
+                tmp['columns'] = []
                 for column in columns:
-                    tmp['columnNames'].append(
+                    tmp['columns'].append(
                         dict((str(k),str(v)) for k,v in iteritems(column))
                     )
                 # You cannot have rows without columns
@@ -222,18 +221,6 @@ class Parameters(AnsibleF5Parameters):
                 if rows:
                     tmp['rows'] = list(list())
                     for row in rows:
-                        # This looks weird, but iWorkflow puts the "many" rows
-                        # into a single row. The actual payload looks like this
-                        #
-                        # "rows": [
-                        #   [
-                        #     "12.0.1.11",
-                        #     "80"
-                        #   ],
-                        #   [
-                        #     "12.0.1.12"
-                        #   ]
-                        # ]
                         tmp['rows'][0].append([str(x) for x in row])
             description = table.get('description', None)
             if description:
