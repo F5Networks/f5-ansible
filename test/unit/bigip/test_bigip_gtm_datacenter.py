1412e9dddff4b45d8171fe7a015ca83ac0c7589f
Tim Rupp <caphrim007@gmail.com>
2017-05-11 20:16:33 -0700

    added unit tests for gtm datacenter

---
+++ test/unit/bigip/test_bigip_gtm_datacenter.py
@@ -0,0 +1,269 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 F5 Networks Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import json
+
+from ansible.compat.tests import unittest
+from ansible.compat.tests.mock import patch, Mock
+from ansible.module_utils import basic
+from ansible.module_utils._text import to_bytes
+from ansible.module_utils.f5_utils import (
+    AnsibleF5Client
+)
+
+try:
+    from library.bigip_gtm_datacenter import (
+        Parameters,
+        ModuleManager,
+        ArgumentSpec
+    )
+except ImportError:
+    from ansible.modules.network.f5.bigip_gtm_datacenter import (
+        Parameters,
+        ModuleManager,
+        ArgumentSpec
+    )
+
+fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
+fixture_data = {}
+
+def set_module_args(args):
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
+    basic._ANSIBLE_ARGS = to_bytes(args)
+
+
+def load_fixture(name):
+    path = os.path.join(fixture_path, name)
+
+    if path in fixture_data:
+        return fixture_data[path]
+
+    with open(path) as f:
+        data = f.read()
+
+    try:
+        data = json.loads(data)
+    except Exception:
+        pass
+
+    fixture_data[path] = data
+    return data
+
+
+class TestParameters(unittest.TestCase):
+    def test_module_parameters(self):
+        args = dict(
+            state='present',
+            contact='foo',
+            description='bar',
+            location='baz',
+            name='datacenter'
+        )
+        p = Parameters(args)
+        assert p.state == 'present'
+
+    def test_api_parameters(self):
+        args = load_fixture('load_gtm_datacenter_default.json')
+        p = Parameters(args)
+        assert p.name == 'asd'
+
+    def test_module_parameters_state_present(self):
+        args = dict(
+            state='present'
+        )
+        p = Parameters(args)
+        assert p.state == 'present'
+        assert p.enabled is True
+
+    def test_module_parameters_state_absent(self):
+        args = dict(
+            state='absent'
+        )
+        p = Parameters(args)
+        assert p.state == 'absent'
+
+    def test_module_parameters_state_enabled(self):
+        args = dict(
+            state='enabled'
+        )
+        p = Parameters(args)
+        assert p.state == 'enabled'
+        assert p.enabled is True
+        assert p.disabled is False
+
+    def test_module_parameters_state_disabled(self):
+        args = dict(
+            state='disabled'
+        )
+        p = Parameters(args)
+        assert p.state == 'disabled'
+        assert p.enabled is False
+        assert p.disabled is True
+
+
+@patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
+       return_value=True)
+class TestManager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
+    def test_create_datacenter(self, *args):
+        set_module_args(dict(
+            state='present',
+            password='admin',
+            server='localhost',
+            user='admin',
+            name='foo'
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+        mm = ModuleManager(client)
+
+        # Override methods to force specific logic in the module to happen
+        mm.exists = Mock()
+        mm.exists.side_effect = [False, True]
+        mm.create_on_device = lambda: True
+        mm.exit_json = lambda x: True
+
+        results = mm.exec_module()
+        assert results['changed'] is True
+
+    def test_create_disabled_datacenter(self, *args):
+        set_module_args(dict(
+            state='disabled',
+            password='admin',
+            server='localhost',
+            user='admin',
+            name='foo'
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+        mm = ModuleManager(client)
+
+        # Override methods to force specific logic in the module to happen
+        mm.exists = Mock()
+        mm.exists.side_effect = [False, True]
+        mm.create_on_device = lambda: True
+        mm.exit_json = lambda x: True
+
+        results = mm.exec_module()
+        assert results['changed'] is True
+        assert results['enabled'] is False
+
+    def test_create_enabled_datacenter(self, *args):
+        set_module_args(dict(
+            state='enabled',
+            password='admin',
+            server='localhost',
+            user='admin',
+            name='foo'
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+        mm = ModuleManager(client)
+
+        # Override methods to force specific logic in the module to happen
+        mm.exists = Mock()
+        mm.exists.side_effect = [False, True]
+        mm.create_on_device = lambda: True
+        mm.exit_json = lambda x: True
+
+        results = mm.exec_module()
+        assert results['changed'] is True
+        assert results['enabled'] is True
+
+
+@patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
+       return_value=True)
+class TestLegacyManager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
+    def test_legacy_disable_datacenter(self, *args):
+        set_module_args(dict(
+            state='present',
+            enabled='no',
+            password='admin',
+            server='localhost',
+            user='admin',
+            name='foo'
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+        mm = ModuleManager(client)
+
+        # Override methods to force specific logic in the module to happen
+        mm.exists = Mock()
+        mm.exists.side_effect = [False, True]
+        mm.create_on_device = lambda: True
+        mm.exit_json = lambda x: True
+
+        results = mm.exec_module()
+        assert results['changed'] is True
+        assert results['enabled'] is False
+
+    def test_legacy_enable_datacenter(self, *args):
+        set_module_args(dict(
+            state='present',
+            enabled='yes',
+            password='admin',
+            server='localhost',
+            user='admin',
+            name='foo'
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+        mm = ModuleManager(client)
+
+        # Override methods to force specific logic in the module to happen
+        mm.exists = Mock()
+        mm.exists.side_effect = [False, True]
+        mm.create_on_device = lambda: True
+        mm.exit_json = lambda x: True
+
+        results = mm.exec_module()
+        assert results['changed'] is True
+        assert results['enabled'] is True
