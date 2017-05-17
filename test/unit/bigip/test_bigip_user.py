064227f86b69446d334eebbd2107293c409eea5c
Tim Rupp <caphrim007@gmail.com>
2017-05-17 10:06:27 -0700

    expanding includes

--- test/unit/bigip/test_bigip_user.py
+++ test/unit/bigip/test_bigip_user.py
@@ -29,10 +29,8 @@ from ansible.compat.tests import unittest
 from ansible.compat.tests.mock import patch
 from ansible.module_utils import basic
 from ansible.module_utils._text import to_bytes
-from ansible.module_utils.f5_utils import (
-    AnsibleF5Client,
-    F5ModuleError
-)
+from ansible.module_utils.f5_utils import AnsibleF5Client
+from ansible.module_utils.f5_utils import F5ModuleError
 
 try:
     from library.bigip_user import Parameters
