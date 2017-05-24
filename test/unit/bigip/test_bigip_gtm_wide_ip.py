e1c9189ee3422d16067eceaf6589023e9690e180
Tim Rupp <caphrim007@gmail.com>
2017-05-24 10:29:04 -0700

    adding missing import

--- test/unit/bigip/test_bigip_gtm_wide_ip.py
+++ test/unit/bigip/test_bigip_gtm_wide_ip.py
@@ -31,7 +31,7 @@ import json
 import pytest
 
 from ansible.compat.tests import unittest
-from ansible.compat.tests.mock import patch
+from ansible.compat.tests.mock import patch, Mock
 from ansible.module_utils import basic
 from ansible.module_utils._text import to_bytes
 from ansible.module_utils.f5_utils import AnsibleF5Client
