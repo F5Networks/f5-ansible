299ea0f271f437d9936e9eb228ddf01aa1f06885
Tim Rupp <caphrim007@gmail.com>
2017-07-12 09:47:15 -0700

    added more tests for monitor_tcp

--- test/unit/bigip/test_bigip_monitor_tcp.py
+++ test/unit/bigip/test_bigip_monitor_tcp.py
@@ -22,7 +22,6 @@ __metaclass__ = type
 
 import os
 import json
-import pytest
 import sys
 
 from nose.plugins.skip import SkipTest
@@ -34,7 +33,6 @@ from ansible.compat.tests.mock import patch, Mock
 from ansible.module_utils import basic
 from ansible.module_utils._text import to_bytes
 from ansible.module_utils.f5_utils import AnsibleF5Client
-from ansible.module_utils.f5_utils import F5ModuleError
 
 try:
     from library.bigip_monitor_tcp import Parameters
@@ -76,69 +74,6 @@ def load_fixture(name):
     return data
 
 
-'''
-  name:
-    description:
-      - Monitor name.
-    required: True
-    aliases:
-      - monitor
-  parent:
-    description:
-      - The parent template of this monitor template. Once this value has
-        been set, it cannot be changed. By default, this value is the C(tcp)
-        parent on the C(Common) partition.
-    default: "/Common/tcp"
-  send:
-    description:
-      - The send string for the monitor call.
-  receive:
-    description:
-      - The receive string for the monitor call.
-  ip:
-    description:
-      - IP address part of the ip/port definition. If this parameter is not
-        provided when creating a new monitor, then the default value will be
-        '*'.
-  type:
-    description:
-      - The template type of this monitor template.
-      - Deprecated in 2.4. Use one of the C(bigip_monitor_tcp_echo) or
-        C(migip_monitor_tcp_half_open) instead.
-    default: 'TTYPE_TCP'
-    choices:
-      - TTYPE_TCP
-      - TTYPE_TCP_ECHO
-      - TTYPE_TCP_HALF_OPEN
-  port:
-    description:
-      - Port address part op the ipport definition. If this parameter is not
-        provided when creating a new monitor, then the default value will be
-        '0'.
-  interval:
-    description:
-      - The interval specifying how frequently the monitor instance of this
-        template will run. If this parameter is not provided when creating
-        a new monitor, then the default value will be 5.
-  timeout:
-    description:
-      - The number of seconds in which the node or service must respond to
-        the monitor request. If the target responds within the set time
-        period, it is considered up. If the target does not respond within
-        the set time period, it is considered down. You can change this
-        number to any number you want, however, it should be 3 times the
-        interval number of seconds plus 1 second. If this parameter is not
-        provided when creating a new monitor, then the default value will be 16.
-  time_until_up:
-    description:
-      - Specifies the amount of time in seconds after the first successful
-        response before a node will be marked up. A value of 0 will cause a
-        node to be marked up immediately after a valid response is received
-        from the node. If this parameter is not provided when creating
-        a new monitor, then the default value will be 0.
-'''
-
-
 class TestParameters(unittest.TestCase):
     def test_module_parameters(self):
         args = dict(
@@ -152,6 +87,34 @@ class TestParameters(unittest.TestCase):
             interval=20,
             timeout=30,
             time_until_up=60,
+            partition='Common'
+        )
+
+        p = Parameters(args)
+        assert p.name == 'foo'
+        assert p.parent == '/Common/parent'
+        assert p.send == 'this is a send string'
+        assert p.receive == 'this is a receive string'
+        assert p.ip == '10.10.10.10'
+        assert p.type == 'tcp'
+        assert p.port == 80
+        assert p.destination == '10.10.10.10:80'
+        assert p.interval == 20
+        assert p.timeout == 30
+        assert p.time_until_up == 60
+
+    def test_module_parameters_ints_as_strings(self):
+        args = dict(
+            name='foo',
+            parent='parent',
+            send='this is a send string',
+            receive='this is a receive string',
+            ip='10.10.10.10',
+            type='TTYPE_TCP',
+            port='80',
+            interval='20',
+            timeout='30',
+            time_until_up='60',
             partition='Common'
         )
 
@@ -274,3 +237,195 @@ class TestManager(unittest.TestCase):
         results = mm.exec_module()
 
         assert results['changed'] is False
+
+    def test_update_port(self, *args):
+        set_module_args(dict(
+            name='foo',
+            port=800,
+            partition='Common',
+            server='localhost',
+            password='password',
+            user='admin'
+        ))
+
+        current = Parameters(load_fixture('load_ltm_monitor_tcp.json'))
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        # Override methods in the specific type of manager
+        tm = TcpManager(client)
+        tm.exists = Mock(return_value=True)
+        tm.read_current_from_device = Mock(return_value=current)
+        tm.update_on_device = Mock(return_value=True)
+
+        # Override methods to force specific logic in the module to happen
+        mm = ModuleManager(client)
+        mm.get_manager = Mock(return_value=tm)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
+        assert results['port'] == 800
+
+    def test_update_interval(self, *args):
+        set_module_args(dict(
+            name='foo',
+            interval=200,
+            partition='Common',
+            server='localhost',
+            password='password',
+            user='admin'
+        ))
+
+        current = Parameters(load_fixture('load_ltm_monitor_tcp.json'))
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        # Override methods in the specific type of manager
+        tm = TcpManager(client)
+        tm.exists = Mock(return_value=True)
+        tm.read_current_from_device = Mock(return_value=current)
+        tm.update_on_device = Mock(return_value=True)
+
+        # Override methods to force specific logic in the module to happen
+        mm = ModuleManager(client)
+        mm.get_manager = Mock(return_value=tm)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
+        assert results['interval'] == 200
+
+    def test_update_send(self, *args):
+        set_module_args(dict(
+            name='foo',
+            send='this is another send string',
+            partition='Common',
+            server='localhost',
+            password='password',
+            user='admin'
+        ))
+
+        current = Parameters(load_fixture('load_ltm_monitor_tcp.json'))
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        # Override methods in the specific type of manager
+        tm = TcpManager(client)
+        tm.exists = Mock(return_value=True)
+        tm.read_current_from_device = Mock(return_value=current)
+        tm.update_on_device = Mock(return_value=True)
+
+        # Override methods to force specific logic in the module to happen
+        mm = ModuleManager(client)
+        mm.get_manager = Mock(return_value=tm)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
+        assert results['send'] == 'this is another send string'
+
+    def test_update_receive(self, *args):
+        set_module_args(dict(
+            name='foo',
+            receive='this is another receive string',
+            partition='Common',
+            server='localhost',
+            password='password',
+            user='admin'
+        ))
+
+        current = Parameters(load_fixture('load_ltm_monitor_tcp.json'))
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        # Override methods in the specific type of manager
+        tm = TcpManager(client)
+        tm.exists = Mock(return_value=True)
+        tm.read_current_from_device = Mock(return_value=current)
+        tm.update_on_device = Mock(return_value=True)
+
+        # Override methods to force specific logic in the module to happen
+        mm = ModuleManager(client)
+        mm.get_manager = Mock(return_value=tm)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
+        assert results['receive'] == 'this is another receive string'
+
+    def test_update_timeout(self, *args):
+        set_module_args(dict(
+            name='foo',
+            timeout=300,
+            partition='Common',
+            server='localhost',
+            password='password',
+            user='admin'
+        ))
+
+        current = Parameters(load_fixture('load_ltm_monitor_tcp.json'))
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        # Override methods in the specific type of manager
+        tm = TcpManager(client)
+        tm.exists = Mock(return_value=True)
+        tm.read_current_from_device = Mock(return_value=current)
+        tm.update_on_device = Mock(return_value=True)
+
+        # Override methods to force specific logic in the module to happen
+        mm = ModuleManager(client)
+        mm.get_manager = Mock(return_value=tm)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
+        assert results['timeout'] == 300
+
+    def test_update_time_until_up(self, *args):
+        set_module_args(dict(
+            name='foo',
+            time_until_up=300,
+            partition='Common',
+            server='localhost',
+            password='password',
+            user='admin'
+        ))
+
+        current = Parameters(load_fixture('load_ltm_monitor_tcp.json'))
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        # Override methods in the specific type of manager
+        tm = TcpManager(client)
+        tm.exists = Mock(return_value=True)
+        tm.read_current_from_device = Mock(return_value=current)
+        tm.update_on_device = Mock(return_value=True)
+
+        # Override methods to force specific logic in the module to happen
+        mm = ModuleManager(client)
+        mm.get_manager = Mock(return_value=tm)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
+        assert results['time_until_up'] == 300
