2c9c00f7e48378e3e74edd0ed5d3b73fd0aa4f66
Tim Rupp <caphrim007@gmail.com>
2017-08-11 14:52:25 -0700

    style fixes

--- test/unit/bigip/test_bigip_selfip.py
+++ test/unit/bigip/test_bigip_selfip.py
@@ -139,7 +139,6 @@ class TestParameters(unittest.TestCase):
         assert p.vlan == '/Common/net1'
 
 
-
 @patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
        return_value=True)
 class TestManager(unittest.TestCase):
