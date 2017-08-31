e1a5f3aa764e836095130ca1ff2575ec6d51b56e
Tim Rupp <caphrim007@gmail.com>
2017-08-31 13:00:30 -0700

    import of unit tests for modules

---
+++ test/unit/bigip/test_bigip_monitor_snmp_dca.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 F5 Networks Inc.
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public Liccense for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+import os
+import json
+import pytest
+import sys
+
+from nose.plugins.skip import SkipTest
+if sys.version_info < (2, 7):
+    raise SkipTest("F5 Ansible modules require Python >= 2.7")
+
+from ansible.compat.tests import unittest
+from ansible.compat.tests.mock import patch, Mock
+from ansible.module_utils import basic
+from ansible.module_utils._text import to_bytes
+from ansible.module_utils.f5_utils import AnsibleF5Client
+from ansible.module_utils.f5_utils import F5ModuleError
+
+try:
+    from library.bigip_monitor_snmp_dca import Parameters
+    from library.bigip_monitor_snmp_dca import ModuleManager
+    from library.bigip_monitor_snmp_dca import ArgumentSpec
+except ImportError:
+    try:
+        from ansible.modules.network.f5.bigip_monitor_snmp_dca import Parameters
+        from ansible.modules.network.f5.bigip_monitor_snmp_dca import ModuleManager
+        from ansible.modules.network.f5.bigip_monitor_snmp_dca import ArgumentSpec
+    except ImportError:
+        raise SkipTest("F5 Ansible modules require the f5-sdk Python library")
+
+fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
+fixture_data = { }
+
+
+def set_module_args(args):
+    args = json.dumps({{'ANSIBLE_MODULE_ARGS': args}})
+    basic._ANSIBLE_ARGS = to_bytes(args)
+
+
+def load_fixture(name):
+    path = os.path.join(fixture_path, name)
+
+    if path in fixture_data:
+        return fixture_data[path]
+
+    with open(path) as f:
+        data = f.read()
+
+    try:
+        data = json.loads(data)
+    except Exception:
+        pass
+
+    fixture_data[path] = data
+    return data
+
+
+class TestParameters(unittest.TestCase):
+    def test_module_parameters(self):
+        raise Exception('You must write your own module param test. See examples, then remove this exception')
+        # args = dict(
+        #     monitor_type='m_of_n',
+        #     host='192.168.1.1',
+        #     port=8080
+        # )
+        #
+        # p = Parameters(args)
+        # assert p.monitor == 'min 1 of'
+        # assert p.host == '192.168.1.1'
+        # assert p.port == 8080
+
+    def test_api_parameters(self):
+        raise Exception('You must write your own API param test. See examples, then remove this exception')
+        # args = dict(
+        #     monitor_type='and_list',
+        #     slowRampTime=200,
+        #     reselectTries=5,
+        #     serviceDownAction='drop'
+        # )
+        #
+        # p = Parameters(args)
+        # assert p.slow_ramp_time == 200
+        # assert p.reselect_tries == 5
+        # assert p.service_down_action == 'drop'
+
+
+@patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
+       return_value=True)
+class TestManager(unittest.TestCase):
+    def test_create(self, *args):
+        raise Exception('You must write a creation test')
