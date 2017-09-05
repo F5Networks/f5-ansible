a134fd1005a8d93c88cb29a2597dedc2162a4cc0
Tim Rupp <caphrim007@gmail.com>
2017-09-05 14:08:03 -0700

    added unit tests for snmp dca monitor

--- test/unit/bigip/test_bigip_monitor_snmp_dca.py
+++ test/unit/bigip/test_bigip_monitor_snmp_dca.py
@@ -53,7 +53,7 @@ fixture_data = { }
 
 
 def set_module_args(args):
-    args = json.dumps({{'ANSIBLE_MODULE_ARGS': args}})
+    args = json.dumps({'ANSIBLE_MODULE_ARGS': args})
     basic._ANSIBLE_ARGS = to_bytes(args)
 
 
@@ -77,35 +77,115 @@ def load_fixture(name):
 
 class TestParameters(unittest.TestCase):
     def test_module_parameters(self):
-        raise Exception('You must write your own module param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='m_of_n',
-        #     host='192.168.1.1',
-        #     port=8080
-        # )
-        #
-        # p = Parameters(args)
-        # assert p.monitor == 'min 1 of'
-        # assert p.host == '192.168.1.1'
-        # assert p.port == 8080
+        args = dict(
+            agent_type='UCD',
+            community='public',
+            cpu_coefficient='1.5',
+            cpu_threshold='80',
+            parent='/Common/snmp_dca',
+            disk_coefficient='2.0',
+            disk_threshold='90',
+            interval=10,
+            memory_coefficient='1.0',
+            memory_threshold='70',
+            time_until_up=0,
+            timeout=30,
+            version='v1'
+        )
+
+        p = Parameters(args)
+        assert p.agent_type == 'UCD'
+        assert p.community == 'public'
+        assert p.cpu_coefficient == 1.5
+        assert p.cpu_threshold == 80
+        assert p.parent == '/Common/snmp_dca'
+        assert p.disk_coefficient == 2.0
+        assert p.disk_threshold == 90
+        assert p.interval == 10
+        assert p.memory_coefficient == 1.0
+        assert p.memory_threshold == 70
+        assert p.time_until_up == 0
+        assert p.timeout == 30
+        assert p.version == 'v1'
 
     def test_api_parameters(self):
-        raise Exception('You must write your own API param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='and_list',
-        #     slowRampTime=200,
-        #     reselectTries=5,
-        #     serviceDownAction='drop'
-        # )
-        #
-        # p = Parameters(args)
-        # assert p.slow_ramp_time == 200
-        # assert p.reselect_tries == 5
-        # assert p.service_down_action == 'drop'
+        args = dict(
+            agentType='UCD',
+            community='public',
+            cpuCoefficient='1.5',
+            cpuThreshold='80',
+            defaultsFrom='/Common/snmp_dca',
+            diskCoefficient='2.0',
+            diskThreshold='90',
+            interval=10,
+            memoryCoefficient='1.0',
+            memoryThreshold='70',
+            timeUntilUp=0,
+            timeout=30,
+            apiRawValues={
+                "userDefined asdasd": "{ foo }",
+                "userDefined bar": "tim rupp",
+                "user-defined baz-": "nia",
+                "userDefined userDefined": "23234"
+            },
+            version='v1'
+        )
+
+        p = Parameters(args)
+        assert p.agent_type == 'UCD'
+        assert p.community == 'public'
+        assert p.cpu_coefficient == 1.5
+        assert p.cpu_threshold == 80
+        assert p.parent == '/Common/snmp_dca'
+        assert p.disk_coefficient == 2.0
+        assert p.disk_threshold == 90
+        assert p.interval == 10
+        assert p.memory_coefficient == 1.0
+        assert p.memory_threshold == 70
+        assert p.time_until_up == 0
+        assert p.timeout == 30
+        assert p.version == 'v1'
 
 
 @patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
        return_value=True)
 class TestManager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
     def test_create(self, *args):
-        raise Exception('You must write a creation test')
+        set_module_args(dict(
+            name='foo',
+            agent_type='UCD',
+            community='public',
+            cpu_coefficient='1.5',
+            cpu_threshold='80',
+            parent='/Common/snmp_dca',
+            disk_coefficient='2.0',
+            disk_threshold='90',
+            memory_coefficient='1.0',
+            memory_threshold='70',
+            version='v1',
+            interval=20,
+            timeout=30,
+            time_until_up=60,
+            server='localhost',
+            password='password',
+            user='admin'
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        # Override methods in the specific type of manager
+        mm = ModuleManager(client)
+        mm.exists = Mock(side_effect=[False, True])
+        mm.create_on_device = Mock(return_value=True)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
