221c143770a1f2333ece3d0f82eae0a282eb0a38
Tim Rupp <caphrim007@gmail.com>
2017-09-05 19:13:45 -0700

    fixes to docs

--- library/bigip_monitor_tcp_echo.py
+++ library/bigip_monitor_tcp_echo.py
@@ -27,8 +27,8 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = '''
 ---
 module: bigip_monitor_tcp_echo
-short_description: Manages F5 BIG-IP LTM tcp monitors.
-description: Manages F5 BIG-IP LTM tcp monitors via iControl SOAP API.
+short_description: Manages F5 BIG-IP LTM tcp echo monitors.
+description: Manages F5 BIG-IP LTM tcp echo monitors.
 version_added: "2.4"
 options:
   name:
--- library/bigip_monitor_tcp_half_open.py
+++ library/bigip_monitor_tcp_half_open.py
@@ -27,8 +27,8 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = '''
 ---
 module: bigip_monitor_tcp_half_open
-short_description: Manages F5 BIG-IP LTM tcp monitors.
-description: Manages F5 BIG-IP LTM tcp monitors via iControl SOAP API.
+short_description: Manages F5 BIG-IP LTM tcp half-open monitors.
+description: Manages F5 BIG-IP LTM tcp half-open monitors.
 version_added: "2.4"
 options:
   name:
