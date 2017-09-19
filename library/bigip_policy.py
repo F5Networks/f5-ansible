c0a387959518c66c35ee1f0aa342237d25eeb6fb
Tim Rupp <caphrim007@gmail.com>
2017-09-19 12:21:11 -0700

    added partition docs

--- library/bigip_policy.py
+++ library/bigip_policy.py
@@ -65,6 +65,12 @@ options:
       - all
       - best
       - Custom strategy
+  partition:
+    description:
+      - Device partition to manage resources on.
+    required: False
+    default: 'Common'
+    version_added: 2.5
 notes:
    - Requires the f5-sdk Python package on the host. This is as easy as
      pip install f5-sdk
