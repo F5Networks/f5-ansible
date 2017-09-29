54e19221f01f5e063a7c3349a03cf2089569b874
Tim Rupp <caphrim007@gmail.com>
2017-09-29 16:08:33 -0700

    added unit tests for vcmp guest module

--- test/unit/bigip/test_bigip_vcmp_guest.py
+++ test/unit/bigip/test_bigip_vcmp_guest.py
@@ -77,31 +77,88 @@ def load_fixture(name):
 
 class TestParameters(unittest.TestCase):
     def test_module_parameters(self):
-        raise Exception('You must write your own module param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='m_of_n',
-        #     host='192.168.1.1',
-        #     port=8080
-        # )
-        #
-        # p = Parameters(args)
-        # assert p.monitor == 'min 1 of'
-        # assert p.host == '192.168.1.1'
-        # assert p.port == 8080
+        args = dict(
+            initial_image='BIGIP-12.1.0.1.0.1447-HF1.iso',
+            mgmt_network='bridged',
+            mgmt_address='1.2.3.4/24',
+            vlans=[
+                'vlan1',
+                'vlan2'
+            ]
+        )
+
+        p = Parameters(args)
+        assert p.initial_image == 'BIGIP-12.1.0.1.0.1447-HF1.iso'
+        assert p.mgmt_network == 'bridged'
+
+    def test_module_parameters_mgmt_bridged_without_subnet(self):
+        args = dict(
+            mgmt_network='bridged',
+            mgmt_address='1.2.3.4'
+        )
+
+        p = Parameters(args)
+        assert p.mgmt_network == 'bridged'
+        assert p.mgmt_address == '1.2.3.4/24'
+
+    def test_module_parameters_mgmt_address_cidr(self):
+        args = dict(
+            mgmt_network='bridged',
+            mgmt_address='1.2.3.4/24'
+        )
+
+        p = Parameters(args)
+        assert p.mgmt_network == 'bridged'
+        assert p.mgmt_address == '1.2.3.4/24'
+
+    def test_module_parameters_mgmt_address_subnet(self):
+        args = dict(
+            mgmt_network='bridged',
+            mgmt_address='1.2.3.4/255.255.255.0'
+        )
+
+        p = Parameters(args)
+        assert p.mgmt_network == 'bridged'
+        assert p.mgmt_address == '1.2.3.4/24'
+
+    def test_module_parameters_mgmt_route(self):
+        args = dict(
+            mgmt_address='1.2.3.4'
+        )
+
+        p = Parameters(args)
+        assert p.mgmt_route == '1.2.3.4'
+
+    def test_module_parameters_vcmp_software_image_facts(self):
+        # vCMP images may include a forward slash in their names. This is probably
+        # related to the slots on the system, but it is not a valid value to specify
+        # that slot when providing an initial image
+        args = dict(
+            initial_image='BIGIP-12.1.0.1.0.1447-HF1.iso/1',
+        )
+
+        p = Parameters(args)
+        assert p.initial_image == 'BIGIP-12.1.0.1.0.1447-HF1.iso'
 
     def test_api_parameters(self):
-        raise Exception('You must write your own API param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='and_list',
-        #     slowRampTime=200,
-        #     reselectTries=5,
-        #     serviceDownAction='drop'
-        # )
-        #
-        # p = Parameters(args)
-        # assert p.slow_ramp_time == 200
-        # assert p.reselect_tries == 5
-        # assert p.service_down_action == 'drop'
+        args = dict(
+            initialImage="BIGIP-tmos-tier2-13.1.0.0.0.931.iso",
+            managementGw="2.2.2.2",
+            managementIp="1.1.1.1/24",
+            managementNetwork="bridged",
+            state="deployed",
+            vlans=[
+                "/Common/vlan1",
+                "/Common/vlan2"
+            ]
+        )
+
+        p = Parameters(args)
+        assert p.initial_image == 'BIGIP-tmos-tier2-13.1.0.0.0.931.iso'
+        assert p.mgmt_route == '2.2.2.2'
+        assert p.mgmt_address == '1.1.1.1/24'
+        assert '/Common/vlan1' in p.vlans
+        assert '/Common/vlan2' in p.vlans
 
 
 @patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
