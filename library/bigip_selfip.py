ccc1d8f52c499620ec85df484c86577e0f71f89a
Tim Rupp <caphrim007@gmail.com>
2017-10-04 14:47:28 -0700

    fixed fqdn problem

--- library/bigip_selfip.py
+++ library/bigip_selfip.py
@@ -432,12 +432,9 @@ class Parameters(AnsibleF5Parameters):
         return set(result)
 
     def _fqdn_name(self, value):
-        if value.startswith('/'):
-            name = os.path.basename(value)
-            result = '/{0}/{1}'.format(self.partition, name)
-        else:
-            result = '/{0}/{1}'.format(self.partition, value)
-        return result
+        if value is not None and not value.startswith('/'):
+            return '/{0}/{1}'.format(self.partition, value)
+        return value
 
     @property
     def vlan(self):
