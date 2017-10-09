df4b21864e6d52489a9c2001d144f4dbb83c5fd9
Tim Rupp <caphrim007@gmail.com>
2017-10-09 12:07:02 -0700

    fixes for upstreaming

--- library/bigip_selfip.py
+++ library/bigip_selfip.py
@@ -8,11 +8,9 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 
-ANSIBLE_METADATA = {
-    'status': ['preview'],
-    'supported_by': 'community',
-    'metadata_version': '1.1'
-}
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
 
 DOCUMENTATION = '''
 ---
@@ -71,8 +69,7 @@ options:
         for Self IPs, but the address used may not match any other address used
         by a Self IP. In that sense, Self IPs are not isolated by partitions as
         other resources on a BIG-IP are.
-    required: False
-    default: 'Common'
+    default: Common
     version_added: 2.5
 notes:
   - Requires the f5-sdk Python package on the host. This is as easy as pip
@@ -89,134 +86,134 @@ author:
 EXAMPLES = '''
 - name: Create Self IP
   bigip_selfip:
-      address: "10.10.10.10"
-      name: "self1"
-      netmask: "255.255.255.0"
-      password: "secret"
-      server: "lb.mydomain.com"
-      user: "admin"
-      validate_certs: "no"
-      vlan: "vlan1"
+    address: 10.10.10.10
+    name: self1
+    netmask: 255.255.255.0
+    password: secret
+    server: lb.mydomain.com
+    user: admin
+    validate_certs: no
+    vlan: vlan1
   delegate_to: localhost
 
 - name: Create Self IP with a Route Domain
   bigip_selfip:
-      server: "lb.mydomain.com"
-      user: "admin"
-      password: "secret"
-      validate_certs: "no"
-      name: "self1"
-      address: "10.10.10.10"
-      netmask: "255.255.255.0"
-      vlan: "vlan1"
-      route_domain: "10"
-      allow_service: "default"
+    server: lb.mydomain.com
+    user: admin
+    password: secret
+    validate_certs: no
+    name: self1
+    address: 10.10.10.10
+    netmask: 255.255.255.0
+    vlan: vlan1
+    route_domain: 10
+    allow_service: default
   delegate_to: localhost
 
 - name: Delete Self IP
   bigip_selfip:
-      name: "self1"
-      password: "secret"
-      server: "lb.mydomain.com"
-      state: "absent"
-      user: "admin"
-      validate_certs: "no"
+    name: self1
+    password: secret
+    server: lb.mydomain.com
+    state: absent
+    user: admin
+    validate_certs: no
   delegate_to: localhost
 
 - name: Allow management web UI to be accessed on this Self IP
   bigip_selfip:
-      name: "self1"
-      password: "secret"
-      server: "lb.mydomain.com"
-      state: "absent"
-      user: "admin"
-      validate_certs: "no"
-      allow_service:
-          - "tcp:443"
+    name: self1
+    password: secret
+    server: lb.mydomain.com
+    state: absent
+    user: admin
+    validate_certs: no
+    allow_service:
+      - tcp:443
   delegate_to: localhost
 
 - name: Allow HTTPS and SSH access to this Self IP
   bigip_selfip:
-      name: "self1"
-      password: "secret"
-      server: "lb.mydomain.com"
-      state: "absent"
-      user: "admin"
-      validate_certs: "no"
-      allow_service:
-          - "tcp:443"
-          - "tpc:22"
+    name: self1
+    password: secret
+    server: lb.mydomain.com
+    state: absent
+    user: admin
+    validate_certs: no
+    allow_service:
+      - tcp:443
+      - tcp:22
   delegate_to: localhost
 
 - name: Allow all services access to this Self IP
   bigip_selfip:
-      name: "self1"
-      password: "secret"
-      server: "lb.mydomain.com"
-      state: "absent"
-      user: "admin"
-      validate_certs: "no"
-      allow_service:
-          - all
+    name: self1
+    password: secret
+    server: lb.mydomain.com
+    state: absent
+    user: admin
+    validate_certs: no
+    allow_service:
+      - all
   delegate_to: localhost
 
 - name: Allow only GRE and IGMP protocols access to this Self IP
   bigip_selfip:
-      name: "self1"
-      password: "secret"
-      server: "lb.mydomain.com"
-      state: "absent"
-      user: "admin"
-      validate_certs: "no"
-      allow_service:
-          - gre:0
-          - igmp:0
+    name: self1
+    password: secret
+    server: lb.mydomain.com
+    state: absent
+    user: admin
+    validate_certs: no
+    allow_service:
+      - gre:0
+      - igmp:0
   delegate_to: localhost
 
 - name: Allow all TCP, but no other protocols access to this Self IP
   bigip_selfip:
-      name: "self1"
-      password: "secret"
-      server: "lb.mydomain.com"
-      state: "absent"
-      user: "admin"
-      validate_certs: "no"
-      allow_service:
-          - tcp:0
+    name: self1
+    password: secret
+    server: lb.mydomain.com
+    state: absent
+    user: admin
+    validate_certs: no
+    allow_service:
+      - tcp:0
   delegate_to: localhost
 '''
 
 RETURN = '''
 allow_service:
-    description: Services that allowed via this Self IP
-    returned: changed
-    type: list
-    sample: ['igmp:0','tcp:22','udp:53']
+  description: Services that allowed via this Self IP
+  returned: changed
+  type: list
+  sample: ['igmp:0','tcp:22','udp:53']
 address:
-    description: The address for the Self IP
-    returned: created
-    type: string
-    sample: "192.0.2.10"
+  description: The address for the Self IP
+  returned: created
+  type: string
+  sample: 192.0.2.10
 name:
-    description: The name of the Self IP
-    returned: created, changed and deleted
-    type: string
-    sample: "self1"
+  description: The name of the Self IP
+  returned: created, changed and deleted
+  type: string
+  sample: self1
 netmask:
-    description: The netmask of the Self IP
-    returned: changed and created
-    type: string
-    sample: "255.255.255.0"
+  description: The netmask of the Self IP
+  returned: changed and created
+  type: string
+  sample: 255.255.255.0
 traffic_group:
-    description: The traffic group that the Self IP is a member of
-    returned: changed and created
-    type: string
-    sample: "traffic-group-local-only"
+  description: The traffic group that the Self IP is a member of
+  returned: changed and created
+  type: string
+  sample: traffic-group-local-only
 vlan:
-    description: The VLAN set on the Self IP
-    returned: changed and created
-    type: string
-    sample: "vlan1"
+  description: The VLAN set on the Self IP
+  returned: changed and created
+  type: string
+  sample: vlan1
 '''
 
 import os
@@ -224,12 +221,12 @@ import re
 
 from ansible.module_utils.f5_utils import AnsibleF5Client
 from ansible.module_utils.f5_utils import AnsibleF5Parameters
+from ansible.module_utils.f5_utils import HAS_F5SDK
 from ansible.module_utils.f5_utils import F5ModuleError
-from ansible.module_utils.f5_utils import defaultdict
-from ansible.module_utils.f5_utils import iteritems
+from ansible.module_utils.six import iteritems
+from collections import defaultdict
 
 try:
-    from ansible.module_utils.f5_utils import HAS_F5SDK
     from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
 except ImportError:
     HAS_F5SDK = False
