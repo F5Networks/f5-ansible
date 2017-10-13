96288dfec5623213af32ff9934986c7ab00b5254
Tim Rupp <caphrim007@gmail.com>
2017-10-12 21:17:50 -0700

    fixed fqdn bug

--- library/bigip_node.py
+++ library/bigip_node.py
@@ -316,12 +316,9 @@ class Parameters(AnsibleF5Parameters):
         return result
 
     def _fqdn_name(self, value):
-        if value.startswith('/'):
-            name = os.path.basename(value)
-            result = '/{0}/{1}'.format(self.partition, name)
-        else:
-            result = '/{0}/{1}'.format(self.partition, value)
-        return result
+        if value is not None and not value.startswith('/'):
+            return '/{0}/{1}'.format(self.partition, value)
+        return value
 
     @property
     def monitors_list(self):
