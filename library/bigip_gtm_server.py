391215cb10cb34acc15e14e691fe0c087d4f78de
Tim Rupp <caphrim007@gmail.com>
2017-10-12 21:18:06 -0700

    fixed fqdn bug

--- library/bigip_gtm_server.py
+++ library/bigip_gtm_server.py
@@ -304,12 +304,9 @@ class Parameters(AnsibleF5Parameters):
         return result
 
     def _fqdn_name(self, value):
-        if value.startswith('/'):
-            name = os.path.basename(value)
-            result = '/{0}/{1}'.format(self.partition, name)
-        else:
-            result = '/{0}/{1}'.format(self.partition, value)
-        return result
+        if value is not None and not value.startswith('/'):
+            return '/{0}/{1}'.format(self.partition, value)
+        return value
 
 
 class Changes(Parameters):
