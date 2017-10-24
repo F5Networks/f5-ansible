b7bf5fba3f9863bd9f6da93b752b967c338a9daf
Tim Rupp <caphrim007@gmail.com>
2017-10-24 11:38:00 -0700

    spacing

--- test/pipeline/ci.f5.f5-ansible-testing.groovy
+++ test/pipeline/ci.f5.f5-ansible-testing.groovy
@@ -10,66 +10,78 @@ timestamps {
     cleanWs()
 
     stage('Checkout') {
-        milestone(label: 'Checkout-AttemptingToStart')
-        lock(resource: 'checkout-${env.BRANCH_NAME}', inversePrecedence: true) {
-            git url: "https://github.com/F5Networks/f5-ansible.git",
-                branch: 'devel'
-        }
-        milestone(label: 'Checkout-Done')
+      milestone(label: 'Checkout-AttemptingToStart')
+      lock(resource: 'checkout-${env.BRANCH_NAME}', inversePrecedence: true) {
+        git url: "https://github.com/F5Networks/f5-ansible.git",
+            branch: 'devel'
+      }
+      milestone(label: 'Checkout-Done')
     }
 
     stage('Integration testing') {
-        milestone(label: 'IntegrationTests-AttemptingToStart')
-        timeout(240) {
-          waitUntil {
-            docker.withRegistry("${DOCKER_REGISTRY}") {
-              docker.image("${DOCKER_IMAGE}").inside {
-                dir('test/runner') {
-                  sh returnStatus: true,
-                     script: """
-                       ansible-playbook -i inventory/hosts playbooks/setup-harness.yaml \
-                       -e harness=${HARNESS} -e harness_name=${HARNESS}-build.${BUILD_NUMBER}
-                     """
-                  return (r == 0)
-                }
-              }
-            }
-          }
-          waitUntil {
-            environment {
-              STACK_NAME = "${HARNESS}-build.${BUILD_NUMBER}"
-            }
-            docker.withRegistry("${DOCKER_REGISTRY}") {
-              docker.image("${DOCKER_IMAGE}").inside {
-                dir('test/integration') {
-                  for (module in modules) {
-                    sh returnStatus: true,
-                       script: """
-                         ansible-playbook -i inventory/stack \
-                         ${module}.yaml -e harness_name=${HARNESS}-build.${BUILD_NUMBER}"
-                       """
-                  }
-                  return (r == 0)
-                }
-              }
-            }
-          }
-          waitUntil {
-            docker.withRegistry("${DOCKER_REGISTRY}") {
-              docker.image("${DOCKER_IMAGE}").inside {
-                dir('test/runner') {
-                  sh returnStatus: true,
-                     script: """
-                       ansible-playbook -i inventory/hosts playbooks/teardown-harness.yaml \
-                       -e harness_name=${HARNESS}-build.${BUILD_NUMBER}
-                     """
-                  return (r == 0)
-                }
+      milestone(label: 'IntegrationTests-AttemptingToStart')
+      timeout(240) {
+        waitUntil {
+          docker.withRegistry("${DOCKER_REGISTRY}") {
+            docker.image("${DOCKER_IMAGE}").inside {
+              dir('test/runner') {
+                sh returnStatus: true,
+                   script: """
+                     ansible-playbook -i inventory/hosts playbooks/setup-harness.yaml \
+                     -e harness=${HARNESS} -e harness_name=${HARNESS}-build.${BUILD_NUMBER}
+                   """
+                return (r == 0)
               }
             }
           }
         }
-        milestone(label: 'IntegrationTests-Done')
+        waitUntil {
+          environment {
+            STACK_NAME = "${HARNESS}-build.${BUILD_NUMBER}"
+          }
+          docker.withRegistry("${DOCKER_REGISTRY}") {
+            docker.image("${DOCKER_IMAGE}").inside {
+              dir('test/integration') {
+                for (module in modules) {
+
+                  def yamlData = readYaml file: './test/integration/'
+                  if !yamlData['vars']['__metadata__'].containsKey('tested_harnesses') {
+                    currentBuild.result = 'SUCCESS'
+                    return
+                  }
+                  isTestableInHarness = ${HARNESS} in yamlData['vars']['__metadata__']['tested_harnesses']
+                  if (!isTestableInHarness) {
+                    currentBuild.result = 'SUCCESS'
+                    return
+                  }
+
+                  sh returnStatus: true,
+                     script: """
+                       ansible-playbook -i inventory/stack \
+                       ${module}.yaml -e harness_name=${HARNESS}-build.${BUILD_NUMBER}"
+                     """
+                }
+                return (r == 0)
+              }
+            }
+          }
+        }
+        waitUntil {
+          docker.withRegistry("${DOCKER_REGISTRY}") {
+            docker.image("${DOCKER_IMAGE}").inside {
+              dir('test/runner') {
+                sh returnStatus: true,
+                   script: """
+                     ansible-playbook -i inventory/hosts playbooks/teardown-harness.yaml \
+                     -e harness_name=${HARNESS}-build.${BUILD_NUMBER}
+                   """
+                return (r == 0)
+              }
+            }
+          }
+        }
+      }
+      milestone(label: 'IntegrationTests-Done')
     }
   }
 }
