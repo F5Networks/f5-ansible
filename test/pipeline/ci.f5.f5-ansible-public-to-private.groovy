f0609a6c07ea5aa1987284134e0b1cf6116d4619
Tim Rupp <caphrim007@gmail.com>
2017-10-27 10:57:53 -0700

    added stages so that jenkins can track time to execute

--- test/pipeline/ci.f5.f5-ansible-public-to-private.groovy
+++ test/pipeline/ci.f5.f5-ansible-public-to-private.groovy
@@ -1,13 +1,17 @@
 node {
+  stage('Cleanup') {
     cleanWs()
+  }
+  stage('Build') {
     docker.image('alpine/git:latest').inside {
-        sh 'mkdir -p ~/.ssh'
-        git branch: 'devel', url: 'https://github.com/F5Networks/f5-ansible.git'
-        sh "git remote add downstream ${DOWNSTREAM_URL}"
-        sshagent(["${CREDENTIAL_ID}"]) {
-            sh "echo 'Host ${IGNORED_HOST}\n\tStrictHostKeyChecking no\n' >> ~/.ssh/config"
-            sh 'git fetch downstream'
-            sh 'git push downstream origin/devel:refs/heads/devel'
-        }
+      sh 'mkdir -p ~/.ssh'
+      git branch: 'devel', url: 'https://github.com/F5Networks/f5-ansible.git'
+      sh "git remote add downstream ${DOWNSTREAM_URL}"
+      sshagent(["${CREDENTIAL_ID}"]) {
+        sh "echo 'Host ${IGNORED_HOST}\n\tStrictHostKeyChecking no\n' >> ~/.ssh/config"
+        sh 'git fetch downstream'
+        sh 'git push downstream origin/devel:refs/heads/devel'
+      }
     }
+  }
 }
