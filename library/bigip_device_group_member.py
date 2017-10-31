6ba83042d6990ec0d1e0b6450936392cd6263ec1
Tim Rupp <caphrim007@gmail.com>
2017-10-31 14:25:43 -0700

    fixed missing import

--- library/bigip_device_group_member.py
+++ library/bigip_device_group_member.py
@@ -77,7 +77,13 @@ from ansible.module_utils.f5_utils import AnsibleF5Client
 from ansible.module_utils.f5_utils import AnsibleF5Parameters
 from ansible.module_utils.f5_utils import HAS_F5SDK
 from ansible.module_utils.f5_utils import F5ModuleError
-from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
+from ansible.module_utils.six import iteritems
+from collections import defaultdict
+
+try:
+    from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
+except ImportError:
+    HAS_F5SDK = False
 
 
 class Parameters(AnsibleF5Parameters):
