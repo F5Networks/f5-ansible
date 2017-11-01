67a3eb7e66aaa1e512aeeb001908ca574c217bb5
Tim Rupp <caphrim007@gmail.com>
2017-10-31 21:33:49 -0700

    fixed import locations

--- library/bigip_virtual_address.py
+++ library/bigip_virtual_address.py
@@ -193,12 +193,12 @@ try:
 except ImportError:
     HAS_NETADDR = False
 
-from ansible.module_utils.basic import BOOLEANS_TRUE
-from ansible.module_utils.basic import BOOLEANS_FALSE
 from ansible.module_utils.f5_utils import AnsibleF5Client
 from ansible.module_utils.f5_utils import AnsibleF5Parameters
 from ansible.module_utils.f5_utils import HAS_F5SDK
 from ansible.module_utils.f5_utils import F5ModuleError
+from ansible.module_utils.parsing.convert_bool import BOOLEANS_TRUE
+from ansible.module_utils.parsing.convert_bool import BOOLEANS_FALSE
 
 try:
     from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
