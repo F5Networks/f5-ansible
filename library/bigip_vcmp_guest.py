19b6db16ae2c9b7fd5455e9bc8c4244e8ba56798
Tim Rupp <caphrim007@gmail.com>
2017-11-06 10:44:47 -0800

    adding remaining work that needed to be done for vcmp guest

--- library/bigip_vcmp_guest.py
+++ library/bigip_vcmp_guest.py
@@ -71,23 +71,35 @@ options:
       - Specifies the IP address, and subnet or subnet mask that you use to access
         the guest when you want to manage a module running within the guest. This
         parameter is required if the C(mgmt_network) parameter is C(bridged).
-      - If you do not specify a network or network mask, a default of C(/24)
-        (C(255.255.255.0)) will be assumed.
+      - When creating a new guest, if you do not specify a network or network mask,
+        a default of C(/24) (C(255.255.255.0)) will be assumed.
   mgmt_route:
     description:
       - Specifies the gateway address for the C(mgmt_address).
+      - If this value is not specified when creating a new guest, it is set to C(none).
+      - The value C(none) can be used during an update to remove this value.
   state:
     description:
-      - The state of the  on the system. When C(present), guarantees
-        that the VLAN exists with the provided attributes. When C(absent),
-        removes the VLAN from the system.
+      - The state of the vCMP guest on the system. Each state implies the actions of
+        all states before it.
+      - When C(configured), guarantees that the vCMP guest exists with the provided
+        attributes. Additionally, ensures that the vCMP guest is turned off.
+      - When C(disabled), behaves the same as C(configured) the name of this state
+        is just a convenience for the user that is more understandable.
+      - When C(provisioned), will ensure that the guest is created and installed.
+        This state will not start the guest; use C(deployed) for that. This state
+        is one step beyond C(present) as C(present) will not install the guest;
+        only setup the configuration for it to be installed.
+      - When C(present), ensures the guest is properly provisioned and starts
+        the guest so that it is in a running state.
+      - When C(absent), removes the vCMP from the system.
     default: "present"
     choices:
+      - configured
       - disabled
       - provisioned
-      - deployed
-      - absent
       - present
+      - absent
 notes:
   - Requires the f5-sdk Python package on the host. This is as easy as pip
     install f5-sdk.
@@ -100,7 +112,7 @@ notes:
     long time (60+ minutes) to reboot and bring all the guests online. The
     BIG-IP chassis will be available before all vCMP guests are online.
 requirements:
-  - f5-sdk >= 2.2.3
+  - f5-sdk >= 3.0.3
   - netaddr
 extends_documentation_fragment: f5
 author:
@@ -132,6 +144,13 @@ EXAMPLES = r'''
       - vlan1
       - vlan2
   delegate_to: localhost
+
+- name: Remove vCMP guest and disk
+  bigip_vcmp_guest:
+    name: guest1
+    state: absent
+    delete_virtual_disk: yes
+  register: result
 '''
 
 RETURN = r'''
@@ -149,7 +168,9 @@ from ansible.module_utils.f5_utils import HAS_F5SDK
 from ansible.module_utils.f5_utils import F5ModuleError
 from ansible.module_utils.six import iteritems
 from collections import defaultdict
+from collections import namedtuple
 
+import time
 
 try:
     from netaddr import IPAddress, AddrFormatError, IPNetwork
@@ -158,6 +179,7 @@ except ImportError:
     HAS_NETADDR = False
 
 try:
+    from f5.utils.responses.handlers import Stats
     from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
 except ImportError:
     HAS_F5SDK = False
@@ -169,10 +191,12 @@ class Parameters(AnsibleF5Parameters):
         'managementNetwork': 'mgmt_network',
         'managementIp': 'mgmt_address',
         'initialImage': 'initial_image',
+        'virtualDisk': 'virtual_disk'
     }
 
     api_attributes = [
-        'vlans', 'managementNetwork', 'managementIp', 'initialImage', 'managementGw'
+        'vlans', 'managementNetwork', 'managementIp', 'initialImage', 'managementGw',
+        'state'
     ]
 
     returnables = [
@@ -180,14 +204,16 @@ class Parameters(AnsibleF5Parameters):
     ]
 
     updatables = [
-        'vlans', 'mgmt_network', 'mgmt_address', 'initial_image', 'mgmt_route'
+        'vlans', 'mgmt_network', 'mgmt_address', 'initial_image', 'mgmt_route',
+        'state'
     ]
 
-    def __init__(self, params=None):
+    def __init__(self, params=None, client=None):
         self._values = defaultdict(lambda: None)
         self._values['__warnings'] = []
         if params:
             self.update(params=params)
+        self.client = client
 
     def update(self, params=None):
         if params:
@@ -213,6 +239,11 @@ class Parameters(AnsibleF5Parameters):
                     # If the mapped value is not a @property
                     self._values[map_key] = v
 
+    def _fqdn_name(self, value):
+        if value is not None and not value.startswith('/'):
+            return '/{0}/{1}'.format(self.partition, value)
+        return value
+
     def to_return(self):
         result = {}
         try:
@@ -237,6 +268,8 @@ class Parameters(AnsibleF5Parameters):
     def mgmt_route(self):
         if self._values['mgmt_route'] is None:
             return None
+        elif self._values['mgmt_route'] == 'none':
+            return 'none'
         try:
             result = IPAddress(self._values['mgmt_route'])
             return str(result)
@@ -249,6 +282,71 @@ class Parameters(AnsibleF5Parameters):
     def mgmt_address(self):
         if self._values['mgmt_address'] is None:
             return None
+        destination = self.mgmt_tuple
+        try:
+            _ = IPAddress(destination.ip)
+            return self._values['mgmt_address']
+        except AddrFormatError:
+            raise F5ModuleError(
+                "The specified 'mgmt_address' is not a valid IP address"
+            )
+
+    @property
+    def mgmt_tuple(self):
+        result = None
+        Destination = namedtuple('Destination', ['ip', 'subnet'])
+        try:
+            parts = self._values['mgmt_address'].split('/')
+            if len(parts) == 2:
+                result = Destination(ip=parts[0], subnet=parts[1])
+            elif len(parts) < 2:
+                result = Destination(ip=parts[0], subnet=None)
+            else:
+                F5ModuleError(
+                    "The provided mgmt_address is malformed."
+                )
+        except ValueError:
+            result = Destination(ip=None, subnet=None)
+        return result
+
+    @property
+    def mgmt_subnet(self):
+        destination = self.mgmt_tuple
+        if destination.subnet is None:
+            return None
+        return int(destination.subnet)
+
+    @property
+    def state(self):
+        if self._values['state'] == 'present':
+            return 'deployed'
+        elif self._values['state'] in ['configured', 'disabled']:
+            return 'configured'
+        return self._values['state']
+
+    @property
+    def vlans(self):
+        if self._values['vlans'] is None:
+            return None
+        result = [self._fqdn_name(x) for x in self._values['vlans']]
+        return result
+
+    @property
+    def initial_image(self):
+        if self._values['initial_image'] is None:
+            return None
+        if self.initial_image_exists(self._values['initial_image']):
+            return self._values['initial_image']
+        raise F5ModuleError(
+            "The specified 'initial_image' does not exist on the remote device"
+        )
+
+    def initial_image_exists(self, image):
+        collection = self.client.api.tm.sys.software.images.get_collection()
+        for resource in collection:
+            if resource.name.startswith(image):
+                return True
+        return False
 
 
 class Changes(Parameters):
@@ -276,11 +374,21 @@ class Difference(object):
         except AttributeError:
             return attr1
 
+    @property
+    def mgmt_address(self):
+        want = self.want.mgmt_tuple
+        if want.subnet is None:
+            raise F5ModuleError(
+                "A subnet must be specified when changing the mgmt_address"
+            )
+        if self.want.mgmt_address != self.have.mgmt_address:
+            return self.want.mgmt_address
+
 
 class ModuleManager(object):
     def __init__(self, client):
         self.client = client
-        self.want = Parameters(self.client.module.params)
+        self.want = Parameters(client=client, params=self.client.module.params)
         self.changes = Changes()
 
     def _set_changed_options(self):
@@ -318,7 +426,7 @@ class ModuleManager(object):
         state = self.want.state
 
         try:
-            if state in ['disabled', 'provisioned', 'deployed', 'present']:
+            if state in ['configured', 'provisioned', 'deployed']:
                 changed = self.present()
             elif state == "absent":
                 changed = self.absent()
@@ -363,6 +471,12 @@ class ModuleManager(object):
         if self.client.check_mode:
             return True
         self.update_on_device()
+        if self.want.state == 'provisioned':
+            self.provision()
+        elif self.want.state == 'deployed':
+            self.deploy()
+        elif self.want.state == 'configured':
+            self.configure()
         return True
 
     def remove(self):
@@ -374,16 +488,24 @@ class ModuleManager(object):
         if self.exists():
             raise F5ModuleError("Failed to delete the resource.")
         if self.want.delete_virtual_disk:
-            self.remove_virtual_disk_from_device()
+            self.remove_virtual_disk()
         return True
 
     def create(self):
         self._set_changed_options()
         if self.client.check_mode:
             return True
+        if self.want.mgmt_tuple.subnet is None:
+            self.want.update(dict(
+                mgmt_address='{0}/255.255.255.0'.format(self.want.mgmt_tuple.ip)
+            ))
         self.create_on_device()
-        if state == 'disabled':
-            self.disable()
+        if self.want.state == 'provisioned':
+            self.provision()
+        elif self.want.state == 'deployed':
+            self.deploy()
+        elif self.want.state == 'configured':
+            self.configure()
         return True
 
     def create_on_device(self):
@@ -419,12 +541,118 @@ class ModuleManager(object):
         result = resource.attrs
         return Parameters(result)
 
+    def remove_virtual_disk(self):
+        if self.virtual_disk_exists():
+            return self.remove_virtual_disk_from_device()
+        return False
+
+    def virtual_disk_exists(self):
+        collection = self.client.api.tm.vcmp.virtual_disks.get_collection()
+        for resource in collection:
+            check = '{0}/'.format(self.have.virtual_disk)
+            if resource.name.startswith(check):
+                return True
+        return False
+
     def remove_virtual_disk_from_device(self):
-        resource = self.client.api.tm.vcmp.guests.guest.load(
-            name=self.want.name
-        )
-        if resource:
-            resource.delete()
+        collection = self.client.api.tm.vcmp.virtual_disks.get_collection()
+        for resource in collection:
+            check = '{0}/'.format(self.have.virtual_disk)
+            if resource.name.startswith(check):
+                resource.delete()
+                return True
+        return False
+
+    def is_configured(self):
+        """Checks to see if guest is disabled
+
+        A disabled guest is fully disabled once their Stats go offline.
+        Until that point they are still in the process of disabling.
+
+        :return:
+        """
+        try:
+            res = self.client.api.tm.vcmp.guests.guest.load(name='guest1')
+            Stats(res.stats.load())
+            return False
+        except iControlUnexpectedHTTPError as ex:
+            if 'Object not found - ' in str(ex):
+                return True
+            raise
+
+    def is_provisioned(self):
+        try:
+            res = self.client.api.tm.vcmp.guests.guest.load(name='guest1')
+            stats = Stats(res.stats.load())
+            if stats.stat['requestedState']['description'] == 'provisioned':
+                if stats.stat['vmStatus']['description'] == 'stopped':
+                    return True
+        except iControlUnexpectedHTTPError:
+            pass
+        return False
+
+    def is_deployed(self):
+        try:
+            res = self.client.api.tm.vcmp.guests.guest.load(name='guest1')
+            stats = Stats(res.stats.load())
+            if stats.stat['requestedState']['description'] == 'deployed':
+                if stats.stat['vmStatus']['description'] == 'running':
+                    return True
+        except iControlUnexpectedHTTPError:
+            pass
+        return False
+
+    def configure(self):
+        if self.is_configured():
+            return False
+        self.configure_on_device()
+        self.wait_for_configured()
+        return True
+
+    def configure_on_device(self):
+        resource = self.client.api.tm.vcmp.guests.guest.load(name=self.want.name)
+        resource.modify(state='configured')
+
+    def wait_for_configured(self):
+        nops = 0
+        while nops < 3:
+            if self.is_configured():
+                nops += 1
+            time.sleep(1)
+
+    def provision(self):
+        if self.is_provisioned():
+            return False
+        self.provision_on_device()
+        self.wait_for_provisioned()
+
+    def provision_on_device(self):
+        resource = self.client.api.tm.vcmp.guests.guest.load(name=self.want.name)
+        resource.modify(state='provisioned')
+
+    def wait_for_provisioned(self):
+        nops = 0
+        while nops < 3:
+            if self.is_provisioned():
+                nops += 1
+            time.sleep(1)
+
+    def deploy(self):
+        if self.is_deployed():
+            return False
+        self.deploy_on_device()
+        self.wait_for_deployed()
+
+    def deploy_on_device(self):
+        resource = self.client.api.tm.vcmp.guests.guest.load(name=self.want.name)
+        resource.modify(state='deployed')
+
+    def wait_for_deployed(self):
+        nops = 0
+        while nops < 3:
+            if self.is_deployed():
+                nops += 1
+            time.sleep(1)
 
 
 class ArgumentSpec(object):
@@ -438,8 +666,8 @@ class ArgumentSpec(object):
             mgmt_route=dict(),
             initial_image=dict(),
             state=dict(
-                default='deployed',
-                choices=['disabled', 'provisioned', 'deployed', 'absent', 'present']
+                default='present',
+                choices=['configured', 'disabled', 'provisioned', 'absent', 'present']
             ),
             delete_virtual_disk=dict(
                 type='bool', default='no'
