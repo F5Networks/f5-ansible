aaf23795d6a5715fd7cad6d62d38cfcb6b388835
Tim Rupp <caphrim007@gmail.com>
2017-11-16 21:10:47 -0800

    fixes to docs tests

--- docs/scripts/test-docs.sh
+++ docs/scripts/test-docs.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
+set -x
+
+: ${DOC_IMG:=f5devcentral/containthedocs:latest}
+
+exec docker run -i \
+  -v $PWD:$PWD --workdir $PWD \
+  -e "LOCAL_USER_ID=$(id -u)" \
+  ${DOC_IMG} /bin/bash -s <<EOF
 set -e
 
 #echo "Installing project dependencies"
-#make requirements
+pip install --user -r requirements.readthedocs.txt
 
 echo "Building docs with Sphinx"
 make -C clean || true
 make docs
 
 echo "Checking grammar and style"
-write-good `find ./docs -name '*.rst'` --passive --so --no-illusion --thereIs --cliches
+write-good \$(find ./docs -name '*.rst') --passive --so --no-illusion --thereIs --cliches
 
 echo "Checking links"
 make -C docs linkcheck
+EOF
--- make.bat
+++ make.bat
@@ -5,7 +5,7 @@ REM Command file for documentation
 if "%1" == "docs" (
 	echo.Building documentation.
 	rm docs/modules/* || true
-	python devtools/bin//plugin_formatter.py --module-dir library/ --template-dir devtools/templates/ --output-dir docs/modules/ -v
+	python devtools/bin/plugin_formatter.py --module-dir library/ --template-dir devtools/templates/ --output-dir docs/modules/ -v
 	cd docs && make html
 	if errorlevel 1 exit /b 1
 	goto end
@@ -14,7 +14,7 @@ if "%1" == "docs" (
 if "%1" == "docker-test" (
 	echo.Running test script in docker.
 	echo.View results below.
-	./scripts/docker-docs.sh ./scripts/test-docs.sh
+	./docs/scripts/docker-docs.sh ./scripts/test-docs.sh
 	if errorlevel 1 exit /b 1
 	goto end
 )
