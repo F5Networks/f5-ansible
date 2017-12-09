b3825fbdae7215f34cb8a85754d9de77fc31ba71
Tim Rupp <caphrim007@gmail.com>
2017-12-09 10:32:48 -0800

    correct yaml errors

--- library/bigip_virtual_server.py
+++ library/bigip_virtual_server.py
@@ -55,8 +55,8 @@ options:
       - In order to maximize utility of this setting, specify the most specific address
         prefixes covering all customer addresses and no others.
       - Specify the IP address in Classless Inter-Domain Routing (CIDR) format: address/prefix,
-        where the prefix length is in bits: for example, for IPv4: 10.0.0.1/32 or 10.0.0.0/24,
-        and for IPv6: ffe1::0020/64 or 2001:ed8:77b5:2:10:10:100:42/64.
+        where the prefix length is in bits. For example, for IPv4, 10.0.0.1/32 or 10.0.0.0/24,
+        and for IPv6, ffe1::0020/64 or 2001:ed8:77b5:2:10:10:100:42/64.
     version_added: 2.5
   port:
     description:
