1058aacd128d3f90ad300cdc30e433c54996d8bb
Tim Rupp <caphrim007@gmail.com>
2017-12-13 14:45:20 -0800

    added unit tests for regkey license module

--- test/unit/test_bigiq_regkey_license.py
+++ test/unit/test_bigiq_regkey_license.py
@@ -22,14 +22,16 @@ from ansible.module_utils.f5_utils import AnsibleF5Client
 from ansible.module_utils.f5_utils import F5ModuleError
 
 try:
-    from library.bigiq_regkey_license import Parameters
+    from library.bigiq_regkey_license import ModuleParameters
+    from library.bigiq_regkey_license import ApiParameters
     from library.bigiq_regkey_license import ModuleManager
     from library.bigiq_regkey_license import ArgumentSpec
     from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
     from test.unit.modules.utils import set_module_args
 except ImportError:
     try:
-        from ansible.modules.network.f5.bigiq_regkey_license import Parameters
+        from ansible.modules.network.f5.bigiq_regkey_license import ModuleParameters
+        from ansible.modules.network.f5.bigiq_regkey_license import ApiParameters
         from ansible.modules.network.f5.bigiq_regkey_license import ModuleManager
         from ansible.modules.network.f5.bigiq_regkey_license import ArgumentSpec
         from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
@@ -61,35 +63,57 @@ def load_fixture(name):
 
 class TestParameters(unittest.TestCase):
     def test_module_parameters(self):
-        raise Exception('You must write your own module param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='m_of_n',
-        #     host='192.168.1.1',
-        #     port=8080
-        # )
-        #
-        # p = Parameters(args)
-        # assert p.monitor == 'min 1 of'
-        # assert p.host == '192.168.1.1'
-        # assert p.port == 8080
+        args = dict(
+            regkey_pool='foo',
+            license_key='XXXX-XXXX-XXXX-XXXX-XXXX',
+            accept_eula=True,
+            description='this is a description'
+        )
+
+        p = ModuleParameters(args)
+        assert p.regkey_pool == 'foo'
+        assert p.license_key == 'XXXX-XXXX-XXXX-XXXX-XXXX'
+        assert p.accept_eula == True
+        assert p.description == 'this is a description'
 
     def test_api_parameters(self):
-        raise Exception('You must write your own API param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='and_list',
-        #     slowRampTime=200,
-        #     reselectTries=5,
-        #     serviceDownAction='drop'
-        # )
-        #
-        # p = Parameters(args)
-        # assert p.slow_ramp_time == 200
-        # assert p.reselect_tries == 5
-        # assert p.service_down_action == 'drop'
+        args = load_fixture('load_regkey_license_key.json')
+
+        p = ApiParameters(args)
+        assert p.description == 'foo bar baz'
 
 
 @patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
        return_value=True)
 class TestManager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
     def test_create(self, *args):
-        raise Exception('You must write a creation test')
+        set_module_args(dict(
+            regkey_pool='foo',
+            license_key='XXXX-XXXX-XXXX-XXXX-XXXX',
+            accept_eula=True,
+            description='this is a description',
+            password='passsword',
+            server='localhost',
+            user='admin'
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+        mm = ModuleManager(client)
+
+        # Override methods to force specific logic in the module to happen
+        mm.exists = Mock(side_effect=[False, True])
+        mm.create_on_device = Mock(return_value=True)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
+        assert results['description'] == 'this is a description'
+
