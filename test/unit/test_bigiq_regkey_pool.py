88f6b83735d1638f4898c5c9c62f4541ace89864
Tim Rupp <caphrim007@gmail.com>
2017-12-15 13:07:46 -0800

    adds unit tests for bigiq regkey license pool

--- test/unit/test_bigiq_regkey_pool.py
+++ test/unit/test_bigiq_regkey_pool.py
@@ -22,13 +22,15 @@ from ansible.module_utils.f5_utils import AnsibleF5Client
 from ansible.module_utils.f5_utils import F5ModuleError
 
 try:
-    from library.bigiq_regkey_pool import Parameters
+    from library.bigiq_regkey_pool import ModuleParameters
+    from library.bigiq_regkey_pool import ApiParameters
     from library.bigiq_regkey_pool import ModuleManager
     from library.bigiq_regkey_pool import ArgumentSpec
     from test.unit.modules.utils import set_module_args
 except ImportError:
     try:
-        from ansible.modules.network.f5.bigiq_regkey_pool import Parameters
+        from ansible.modules.network.f5.bigiq_regkey_pool import ModuleParameters
+        from ansible.modules.network.f5.bigiq_regkey_pool import ApiParameters
         from ansible.modules.network.f5.bigiq_regkey_pool import ModuleManager
         from ansible.modules.network.f5.bigiq_regkey_pool import ArgumentSpec
         from units.modules.utils import set_module_args
@@ -59,35 +61,47 @@ def load_fixture(name):
 
 class TestParameters(unittest.TestCase):
     def test_module_parameters(self):
-        raise Exception('You must write your own module param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='m_of_n',
-        #     host='192.168.1.1',
-        #     port=8080
-        # )
-        #
-        # p = Parameters(args)
-        # assert p.monitor == 'min 1 of'
-        # assert p.host == '192.168.1.1'
-        # assert p.port == 8080
+        args = dict(
+            description='this is a description'
+        )
+
+        p = ModuleParameters(args)
+        assert p.description == 'this is a description'
 
     def test_api_parameters(self):
-        raise Exception('You must write your own API param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='and_list',
-        #     slowRampTime=200,
-        #     reselectTries=5,
-        #     serviceDownAction='drop'
-        # )
-        #
-        # p = Parameters(args)
-        # assert p.slow_ramp_time == 200
-        # assert p.reselect_tries == 5
-        # assert p.service_down_action == 'drop'
+        args = load_fixture('load_regkey_license_pool.json')
+
+        p = ApiParameters(args)
+        assert p.description == 'this is a description'
 
 
 @patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
        return_value=True)
 class TestManager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
     def test_create(self, *args):
-        raise Exception('You must write a creation test')
+        set_module_args(dict(
+            name='foo',
+            description='bar baz',
+            server='localhost',
+            password='password',
+            user='admin'
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        # Override methods in the specific type of manager
+        mm = ModuleManager(client)
+        mm.exists = Mock(return_value=False)
+        mm.create_on_device = Mock(return_value=True)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
