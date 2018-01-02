0a1ac8ce9af49020a68d9d9d37f33dcb354c18d6
Tim Rupp <caphrim007@gmail.com>
2018-01-02 15:24:19 -0800

    adds unit tests for gtm server module

--- test/unit/test_bigip_gtm_server.py
+++ test/unit/test_bigip_gtm_server.py
@@ -22,15 +22,21 @@ from ansible.module_utils.f5_utils import AnsibleF5Client
 from ansible.module_utils.f5_utils import F5ModuleError
 
 try:
-    from library.bigip_gtm_server import Parameters
+    from library.bigip_gtm_server import ApiParameters
+    from library.bigip_gtm_server import ModuleParameters
     from library.bigip_gtm_server import ModuleManager
+    from library.bigip_gtm_server import V1Manager
+    from library.bigip_gtm_server import V2Manager
     from library.bigip_gtm_server import ArgumentSpec
     from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
     from test.unit.modules.utils import set_module_args
 except ImportError:
     try:
-        from ansible.modules.network.f5.bigip_gtm_server import Parameters
+        from ansible.modules.network.f5.bigip_gtm_server import ApiParameters
+        from ansible.modules.network.f5.bigip_gtm_server import ModuleParameters
         from ansible.modules.network.f5.bigip_gtm_server import ModuleManager
+        from ansible.modules.network.f5.bigip_gtm_server import V1Manager
+        from ansible.modules.network.f5.bigip_gtm_server import V2Manager
         from ansible.modules.network.f5.bigip_gtm_server import ArgumentSpec
         from ansible.module_utils.f5_utils import iControlUnexpectedHTTPError
         from units.modules.utils import set_module_args
@@ -61,35 +67,224 @@ def load_fixture(name):
 
 class TestParameters(unittest.TestCase):
     def test_module_parameters(self):
-        raise Exception('You must write your own module param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='m_of_n',
-        #     host='192.168.1.1',
-        #     port=8080
-        # )
-        #
-        # p = Parameters(args)
-        # assert p.monitor == 'min 1 of'
-        # assert p.host == '192.168.1.1'
-        # assert p.port == 8080
+        args = dict(
+            name='GTM_Server',
+            datacenter='New York',
+            partition='Common',
+            server_type='bigip',
+            link_discovery=False,
+            virtual_server_discovery=False,
+            devices=[
+                dict(
+                    name='server_1',
+                    address='1.1.1.1'
+                ),
+                dict(
+                    name='server_2',
+                    address='2.2.2.1',
+                    translation='192.168.2.1'
+                ),
+                dict(
+                    name='server_2',
+                    address='2.2.2.2'
+                ),
+                dict(
+                    name='server_3',
+                    addresses=[
+                        dict(
+                            address='3.3.3.1'
+                        ),
+                        dict(
+                            address='3.3.3.2'
+                        )
+                    ]
+                ),
+                dict(
+                    name='server_4',
+                    addresses=[
+                        dict(
+                            address='4.4.4.1',
+                            translation='192.168.14.1'
+                        ),
+                        dict(
+                            address='4.4.4.2'
+                        )
+                    ]
+                )
+            ]
+        )
+
+        p = ModuleParameters(args)
+        assert p.name == 'GTM_Server'
+        assert p.datacenter == '/Common/New York'
+        assert p.server_type == 'bigip'
+        assert p.link_discovery == 'disabled'
+        assert p.virtual_server_discovery == 'disabled'
 
     def test_api_parameters(self):
-        raise Exception('You must write your own API param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='and_list',
-        #     slowRampTime=200,
-        #     reselectTries=5,
-        #     serviceDownAction='drop'
-        # )
-        #
-        # p = Parameters(args)
-        # assert p.slow_ramp_time == 200
-        # assert p.reselect_tries == 5
-        # assert p.service_down_action == 'drop'
+        args = load_fixture('load_gtm_server_1.json')
+
+        p = ApiParameters(args)
+        assert p.name == 'baz'
+        assert p.datacenter == '/Common/foo'
+        assert p.server_type == 'bigip'
+        assert p.link_discovery == 'disabled'
+        assert p.virtual_server_discovery == 'disabled'
 
 
 @patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
        return_value=True)
-class TestManager(unittest.TestCase):
+class TestV1Manager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
     def test_create(self, *args):
-        raise Exception('You must write a creation test')
+        set_module_args(dict(
+            server='lb.mydomain.com',
+            user='admin',
+            password='secret',
+            name='GTM_Server',
+            datacenter='/Common/New York',
+            server_type='bigip',
+            link_discovery='no',
+            virtual_server_discovery='no',
+            devices=[
+                dict(
+                    name='server_1',
+                    address='1.1.1.1'
+                ),
+                dict(
+                    name='server_2',
+                    address='2.2.2.1',
+                    translation='192.168.2.1'
+                ),
+                dict(
+                    name='server_2',
+                    address='2.2.2.2'
+                ),
+                dict(
+                    name='server_3',
+                    addresses=[
+                        dict(
+                            address='3.3.3.1'
+                        ),
+                        dict(
+                            address='3.3.3.2'
+                        )
+                    ]
+                ),
+                dict(
+                    name='server_4',
+                    addresses=[
+                        dict(
+                            address='4.4.4.1',
+                            translation='192.168.14.1'
+                        ),
+                        dict(
+                            address='4.4.4.2'
+                        )
+                    ]
+                )
+            ]
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        m1 = V1Manager(client)
+        m1.exists = Mock(side_effect=[False, True])
+        m1.create_on_device = Mock(return_value=True)
+
+        # Override methods in the specific type of manager
+        mm = ModuleManager(client)
+        mm.get_manager = Mock(return_value=m1)
+        mm.version_is_less_than = Mock(return_value=True)
+        mm.gtm_provisioned = Mock(return_value=True)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
+        assert results['server_type'] == 'bigip'
+
+
+@patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
+       return_value=True)
+class TestV2Manager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
+    def test_create(self, *args):
+        set_module_args(dict(
+            server='lb.mydomain.com',
+            user='admin',
+            password='secret',
+            name='GTM_Server',
+            datacenter='/Common/New York',
+            server_type='bigip',
+            link_discovery='no',
+            virtual_server_discovery='no',
+            devices=[
+                dict(
+                    name='server_1',
+                    address='1.1.1.1'
+                ),
+                dict(
+                    name='server_2',
+                    address='2.2.2.1',
+                    translation='192.168.2.1'
+                ),
+                dict(
+                    name='server_2',
+                    address='2.2.2.2'
+                ),
+                dict(
+                    name='server_3',
+                    addresses=[
+                        dict(
+                            address='3.3.3.1'
+                        ),
+                        dict(
+                            address='3.3.3.2'
+                        )
+                    ]
+                ),
+                dict(
+                    name='server_4',
+                    addresses=[
+                        dict(
+                            address='4.4.4.1',
+                            translation='192.168.14.1'
+                        ),
+                        dict(
+                            address='4.4.4.2'
+                        )
+                    ]
+                )
+            ]
+        ))
+
+        client = AnsibleF5Client(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode,
+            f5_product_name=self.spec.f5_product_name
+        )
+
+        m1 = V2Manager(client)
+        m1.exists = Mock(side_effect=[False, True])
+        m1.create_on_device = Mock(return_value=True)
+
+        # Override methods in the specific type of manager
+        mm = ModuleManager(client)
+        mm.get_manager = Mock(return_value=m1)
+        mm.version_is_less_than = Mock(return_value=False)
+        mm.gtm_provisioned = Mock(return_value=True)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
+        assert results['server_type'] == 'bigip'
