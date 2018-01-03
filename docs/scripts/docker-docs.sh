bd9027350b06a715eb100f080898681e6743ded4
Tim Rupp <caphrim007@gmail.com>
2018-01-02 16:18:59 -0800

    changing local user id to work on shippable

--- docs/scripts/docker-docs.sh
+++ docs/scripts/docker-docs.sh
@@ -4,7 +4,12 @@ set -x
 
 : ${DOC_IMG:=f5devcentral/containthedocs:latest}
 
+LOCAL_USER_ID=`id -u`
+if [ $LOCAL_USER_ID -eq 0 ]; then
+    LOCAL_USER_ID=10443
+fi
+
 exec docker run -i \
   -v $PWD:$PWD --workdir $PWD \
-  -e "LOCAL_USER_ID=$(id -u)" \
+  -e "LOCAL_USER_ID=${LOCAL_USER_ID}" \
   ${DOC_IMG} "$@"
--- docs/scripts/test-docs.sh
+++ docs/scripts/test-docs.sh
@@ -4,6 +4,11 @@ set -x
 
 : ${DOC_IMG:=f5devcentral/containthedocs:latest}
 
+LOCAL_USER_ID=`id -u`
+if [ $LOCAL_USER_ID -eq 0 ]; then
+    LOCAL_USER_ID=10443
+fi
+
 exec docker run -i \
   -v $PWD:$PWD --workdir $PWD \
   -e "LOCAL_USER_ID=$(id -u)" \
