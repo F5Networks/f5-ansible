1458da7021a8238e5ecee0d9759990728b5e452b
Tim Rupp <caphrim007@gmail.com>
2018-01-05 12:07:45 -0800

    added test to check for incorrect comparisons

---
+++ test/ansible/sanity/incorrect-comparisons.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Copyright (c) 2017 F5 Networks Inc.
+# GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+found=''
+
+findings=$(egrep "if self.(\w+).(\w+) (!=|==|>=|=<) self.\1.\2" library/ -R)
+if [ "$findings" ]; then
+    echo "${findings}"
+    found=1
+fi
+
+if [ "${found}" ]; then
+    echo "One or more file(s) listed above contain incorrect want comparisons."
+    exit 1
+fi
