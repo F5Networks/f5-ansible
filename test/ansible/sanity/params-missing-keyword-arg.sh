64ff90f7cdf27af34a9b841845ed598e9c9f16bf
Tim Rupp <caphrim007@gmail.com>
2018-01-10 21:38:56 -0800

    more sanity tests

---
+++ test/ansible/sanity/params-missing-keyword-arg.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#
+# Copyright (c) 2017 F5 Networks Inc.
+# GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+
+found=''
+
+findings=$(egrep "self.want = Parameters\(self.module.params\)" library/ -R)
+if [ "$findings" ]; then
+    echo "${findings}"
+    found=1
+fi
+
+findings=$(egrep "Changes\(changed" library/ -R)
+if [ "$findings" ]; then
+    echo "${findings}"
+    found=1
+fi
+
+findings=$(egrep "ReportableChanges\(self.changes." library/ -R)
+if [ "$findings" ]; then
+    echo "${findings}"
+    found=1
+fi
+
+findings=$(egrep "ApiParameters\(result\)" library/ -R)
+if [ "$findings" ]; then
+    echo "${findings}"
+    found=1
+fi
+
+findings=$(egrep "Parameters\(dict\(d" library/ -R)
+if [ "$findings" ]; then
+    echo "${findings}"
+    found=1
+fi
+
+findings=$(egrep "ApiParameters\(changed\)" library/ -R)
+if [ "$findings" ]; then
+    echo "${findings}"
+    found=1
+fi
+
+findings=$(egrep "Parameters\(resource.attrs\)" library/ -R)
+if [ "$findings" ]; then
+    echo "${findings}"
+    found=1
+fi
+
+findings=$(egrep "Parameters\(changed\)" library/ -R)
+if [ "$findings" ]; then
+    echo "${findings}"
+    found=1
+fi
+
+findings=$(egrep "Parameters\(result\)" library/ -R)
+if [ "$findings" ]; then
+    echo "${findings}"
+    found=1
+fi
+
+if [ "${found}" ]; then
+    echo "One or more file(s) listed above contain a Parameters without keywords."
+    exit 1
+fi
