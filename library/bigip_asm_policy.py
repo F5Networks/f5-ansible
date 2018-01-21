9cba2827afc73795cce928570140ee65f35549fc
Tim Rupp <caphrim007@gmail.com>
2018-01-20 22:00:43 -0800

    fixes to make templates work on < 13.x. Added retries in the case that bigip drops a connection during ASM work.

--- library/bigip_asm_policy.py
+++ library/bigip_asm_policy.py
@@ -540,7 +540,6 @@ class BaseManager(object):
         return False
 
     def create(self):
-        task = None
         if self.want.active is None:
             self.want.update(dict(active=False))
         if self._file_is_missing():
@@ -555,13 +554,9 @@ class BaseManager(object):
             self.create_blank()
         else:
             if self.want.template is not None:
-                task = self.create_from_template_on_device()
+                self.create_from_template()
             elif self.want.file is not None:
-                task = self.import_to_device()
-            if not task:
-                return False
-            if not self.wait_for_task(task):
-                raise F5ModuleError('Import policy task failed.')
+                self.create_from_file()
 
         if self.want.active:
             self.activate()
@@ -669,6 +664,7 @@ class BaseManager(object):
             partition=self.want.partition,
             policyTemplateReference=self.want.template_link
         )
+        time.sleep(2)
         return result
 
     def create_on_device(self):
@@ -720,35 +716,20 @@ class V1Manager(BaseManager):
         super(V1Manager, self).__init__(client=client, module=module)
         self.want = V1Parameters(params=module.params, client=client)
 
-    def create(self):
-        if self.want.active is None:
-            self.want.update(dict(active=False))
-        if self._file_is_missing():
-            raise F5ModuleError(
-                "The specified ASM policy file does not exist"
-            )
-        self._set_changed_options()
-        if self.module.check_mode:
-            return True
+    def create_from_file(self):
+        self.import_to_device()
+        self.remove_temp_policy_from_device()
 
-        if self.want.template is None and self.want.file is None:
-            self.create_blank()
-        else:
-            if self.want.template is not None:
-                task = self.create_from_template_on_device()
-                if not task:
-                    return False
-                if not self.wait_for_task(task):
-                    raise F5ModuleError('Import policy task failed.')
-            elif self.want.file is not None:
-                self.import_to_device()
-                self.remove_temp_policy_from_device()
+    def create_from_template(self):
+        self.create_from_template_on_device()
 
-        if self.want.active:
-            self.activate()
-            return True
-        else:
-            return True
+    def create_from_template_on_device(self):
+        full_name = fqdn_name(self.want.partition, self.want.name)
+        cmd = 'tmsh create asm policy {0} policy-template {1}'.format(full_name, self.want.template)
+        self.client.api.tm.util.bash.exec_cmd(
+            'run',
+            utilCmdArgs='-c "{0}"'.format(cmd)
+        )
 
     def remove_temp_policy_from_device(self):
         name = os.path.split(self.want.file)[1]
@@ -774,6 +755,20 @@ class V2Manager(BaseManager):
         module = kwargs.get('module', None)
         super(V2Manager, self).__init__(client=client, module=module)
         self.want = V2Parameters(params=module.params, client=client)
+
+    def create_from_template(self):
+        task = self.create_from_template_on_device()
+        if not task:
+            return False
+        if not self.wait_for_task(task):
+            raise F5ModuleError('Import policy task failed.')
+
+    def create_from_file(self):
+        task = self.import_to_device()
+        if not task:
+            return False
+        if not self.wait_for_task(task):
+            raise F5ModuleError('Import policy task failed.')
 
 
 class ArgumentSpec(object):
--- library/module_utils/network/f5/bigiq.py
+++ library/module_utils/network/f5/bigiq.py
@@ -25,18 +25,21 @@ except ImportError:
 class F5Client(F5BaseClient):
     @property
     def api(self):
-        try:
-            result = ManagementRoot(
-                self.params['server'],
-                self.params['user'],
-                self.params['password'],
-                port=self.params['server_port'],
-                verify=self.params['validate_certs'],
-                token='local'
-            )
-        except Exception:
+        for x in range(0, 10):
+            try:
+                result = ManagementRoot(
+                    self.params['server'],
+                    self.params['user'],
+                    self.params['password'],
+                    port=self.params['server_port'],
+                    verify=self.params['validate_certs'],
+                    token='local'
+                )
+                return result
+            except Exception:
+                time.sleep(3)
+        else:
             raise F5ModuleError(
                 'Unable to connect to {0} on port {1}. '
                 'Is "validate_certs" preventing this?'.format(self.params['server'], self.params['server_port'])
             )
-        return result
