3eff0a10b12690035b9916f90b35af25cd072f47
Tim Rupp <caphrim007@gmail.com>
2018-01-23 20:13:14 -0800

    added load params to make provider work correctly

--- library/module_utils/network/f5/common.py
+++ library/module_utils/network/f5/common.py
@@ -94,6 +94,14 @@ def get_provider_argspec():
     return f5_provider_spec
 
 
+def load_params(params):
+    provider = params.get('provider') or dict()
+    for key, value in iteritems(provider):
+        if key in f5_argument_spec:
+            if params.get(key) is None and value is not None:
+                params[key] = value
+
+
 # Fully Qualified name (with the partition)
 def fqdn_name(partition, value):
     if value is not None and not value.startswith('/'):
@@ -163,6 +171,7 @@ class Noop(object):
 class F5BaseClient(object):
     def __init__(self, *args, **kwargs):
         self.params = kwargs
+        load_params(self.params)
 
     @property
     def api(self):
