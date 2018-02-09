1f719e1ac89022c02d860d08831383ed5ae3df03
Tim Rupp <caphrim007@gmail.com>
2018-02-08 18:00:52 -0800

    removing quotes

--- devtools/stubs/library_module.py
+++ devtools/stubs/library_module.py
@@ -18,7 +18,7 @@ module: {{ module }}
 short_description: __SHORT_DESCRIPTION__
 description:
   - __LONG DESCRIPTION__.
-version_added: "2.6"
+version_added: 2.6
 options:
   name:
     description:
