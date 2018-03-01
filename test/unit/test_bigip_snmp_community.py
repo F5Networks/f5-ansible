93bbf9d63f83c61825dca94d514e3584b82cc3e0
Tim Rupp <caphrim007@gmail.com>
2018-02-28 19:01:16 -0800

    Fixes: #591

--- test/unit/test_bigip_snmp_community.py
+++ test/unit/test_bigip_snmp_community.py
@@ -24,6 +24,8 @@ try:
     from library.bigip_snmp_community import ApiParameters
     from library.bigip_snmp_community import ModuleParameters
     from library.bigip_snmp_community import ModuleManager
+    from library.bigip_snmp_community import V1Manager
+    from library.bigip_snmp_community import V2Manager
     from library.bigip_snmp_community import ArgumentSpec
     from library.module_utils.network.f5.common import F5ModuleError
     from library.module_utils.network.f5.common import iControlUnexpectedHTTPError
@@ -33,6 +35,8 @@ except ImportError:
         from ansible.modules.network.f5.bigip_snmp_community import ApiParameters
         from ansible.modules.network.f5.bigip_snmp_community import ModuleParameters
         from ansible.modules.network.f5.bigip_snmp_community import ModuleManager
+        from ansible.modules.network.f5.bigip_snmp_community import V1Manager
+        from ansible.modules.network.f5.bigip_snmp_community import V2Manager
         from ansible.modules.network.f5.bigip_snmp_community import ArgumentSpec
         from ansible.module_utils.network.f5.common import F5ModuleError
         from ansible.module_utils.network.f5.common import iControlUnexpectedHTTPError
@@ -64,35 +68,223 @@ def load_fixture(name):
 
 class TestParameters(unittest.TestCase):
     def test_module_parameters(self):
-        raise SkipTest('You must write your own module param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='m_of_n',
-        #     host='192.168.1.1',
-        #     port=8080
-        # )
-        #
-        # p = ModuleParameters(params=args)
-        # assert p.monitor == 'min 1 of'
-        # assert p.host == '192.168.1.1'
-        # assert p.port == 8080
+        args = dict(
+            version='v2c',
+            community='foo',
+            source='1.1.1.1',
+            port='8080',
+            oid='.1',
+            access='ro',
+            ip_version=4,
+            snmp_username='admin',
+            snmp_auth_protocol='sha',
+            snmp_auth_password='secretsecret',
+            snmp_privacy_protocol='des',
+            snmp_privacy_password='secretsecret',
+            update_password='always',
+            state='present'
+        )
 
-    def test_api_parameters(self):
-        raise SkipTest('You must write your own API param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='and_list',
-        #     slowRampTime=200,
-        #     reselectTries=5,
-        #     serviceDownAction='drop'
-        # )
-        #
-        # p = ApiParameters(params=args)
-        # assert p.slow_ramp_time == 200
-        # assert p.reselect_tries == 5
-        # assert p.service_down_action == 'drop'
+        p = ModuleParameters(params=args)
+        assert p.version == 'v2c'
+        assert p.community == 'foo'
+        assert p.source == '1.1.1.1'
+        assert p.port == 8080
+        assert p.oid == '.1'
+        assert p.access == 'ro'
+        assert p.ip_version == 4
+        assert p.snmp_username == 'admin'
+        assert p.snmp_auth_protocol == 'sha'
+        assert p.snmp_auth_password == 'secretsecret'
+        assert p.snmp_privacy_protocol == 'des'
+        assert p.snmp_privacy_password == 'secretsecret'
+        assert p.update_password == 'always'
+        assert p.state == 'present'
+
+    def test_api_parameters_community_1(self):
+        args = load_fixture('load_sys_snmp_communities_1.json')
+
+        p = ApiParameters(params=args)
+        assert p.access == 'ro'
+        assert p.community == 'foo'
+        assert p.ip_version == 4
+
+    def test_api_parameters_community_2(self):
+        args = load_fixture('load_sys_snmp_communities_2.json')
+
+        p = ApiParameters(params=args)
+        assert p.access == 'rw'
+        assert p.community == 'foo'
+        assert p.ip_version == 4
+        assert p.oid == '.1'
+        assert p.source == '1.1.1.1'
+
+    def test_api_parameters_community_3(self):
+        args = load_fixture('load_sys_snmp_communities_3.json')
+
+        p = ApiParameters(params=args)
+        assert p.access == 'ro'
+        assert p.community == 'foo'
+        assert p.ip_version == 6
+        assert p.oid == '.1'
+        assert p.source == '2001:0db8:85a3:0000:0000:8a2e:0370:7334'
+
+    def test_api_parameters_community_4(self):
+        args = load_fixture('load_sys_snmp_communities_4.json')
+
+        p = ApiParameters(params=args)
+        assert p.access == 'ro'
+        assert p.community == 'foo'
+        assert p.ip_version == 6
+
+    def test_api_parameters_users_1(self):
+        args = load_fixture('load_sys_snmp_users_1.json')
+
+        p = ApiParameters(params=args)
+        assert p.access == 'ro'
+        assert p.snmp_auth_protocol == 'sha'
+        assert p.oid == '.1'
+        assert p.snmp_privacy_protocol == 'aes'
+        assert p.snmp_username == 'foo'
 
 
 @patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
        return_value=True)
 class TestManager(unittest.TestCase):
-    def test_create(self, *args):
-        raise SkipTest('You must write a creation test')
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
+    def test_create_v2c_community_1(self, *args):
+        set_module_args(dict(
+            version='v2c',
+            community='foo',
+            source='1.1.1.1',
+            port='8080',
+            oid='.1',
+            access='ro',
+            ip_version=4,
+            state='present',
+            partition='Common',
+            password='password',
+            server='localhost',
+            user='admin'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+        m1 = V1Manager(module=module)
+
+        # Override methods to force specific logic in the module to happen
+        m1.exists = Mock(side_effect=[False, True])
+        m1.create_on_device = Mock(return_value=True)
+
+        m0 = ModuleManager(module=module)
+        m0.get_manager = Mock(return_value=m1)
+
+        results = m0.exec_module()
+
+        assert results['changed'] is True
+
+    def test_create_v1_community_1(self, *args):
+        set_module_args(dict(
+            version='v1',
+            community='foo',
+            source='1.1.1.1',
+            port='8080',
+            oid='.1',
+            access='ro',
+            ip_version=4,
+            state='present',
+            partition='Common',
+            password='password',
+            server='localhost',
+            user='admin'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+        m1 = V1Manager(module=module)
+
+        # Override methods to force specific logic in the module to happen
+        m1.exists = Mock(side_effect=[False, True])
+        m1.create_on_device = Mock(return_value=True)
+
+        m0 = ModuleManager(module=module)
+        m0.get_manager = Mock(return_value=m1)
+
+        results = m0.exec_module()
+
+        assert results['changed'] is True
+
+    def test_create_v3_community_1(self, *args):
+        set_module_args(dict(
+            version='v3',
+            oid='.1',
+            access='ro',
+            snmp_username='admin',
+            snmp_auth_protocol='md5',
+            snmp_auth_password='secretsecret',
+            snmp_privacy_protocol='des',
+            snmp_privacy_password='secretsecret',
+            state='present',
+            partition='Common',
+            password='password',
+            server='localhost',
+            user='admin'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+        m1 = V2Manager(module=module)
+
+        # Override methods to force specific logic in the module to happen
+        m1.exists = Mock(side_effect=[False, True])
+        m1.create_on_device = Mock(return_value=True)
+
+        m0 = ModuleManager(module=module)
+        m0.get_manager = Mock(return_value=m1)
+
+        results = m0.exec_module()
+
+        assert results['changed'] is True
+
+    def test_create_v3_community_2(self, *args):
+        set_module_args(dict(
+            version='v3',
+            access='ro',
+            snmp_username='admin',
+            snmp_auth_protocol='md5',
+            snmp_auth_password='secretsecret',
+            snmp_privacy_protocol='des',
+            snmp_privacy_password='secretsecret',
+            state='present',
+            partition='Common',
+            password='password',
+            server='localhost',
+            user='admin'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+        m1 = V2Manager(module=module)
+
+        # Override methods to force specific logic in the module to happen
+        m1.exists = Mock(side_effect=[False, True])
+        m1.create_on_device = Mock(return_value=True)
+
+        m0 = ModuleManager(module=module)
+        m0.get_manager = Mock(return_value=m1)
+
+        with pytest.raises(F5ModuleError) as ex:
+            m0.exec_module()
+
+        assert 'oid must be specified when creating a new v3 community.' == str(ex.value)
