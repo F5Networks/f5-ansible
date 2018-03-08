d7e9d8144d8cbaca4a618e4894b7755144d24a18
Tim Rupp <caphrim007@gmail.com>
2018-03-08 07:52:19 -0800

    removed incorrect description text

--- library/bigip_gtm_monitor_tcp_half_open.py
+++ library/bigip_gtm_monitor_tcp_half_open.py
@@ -34,8 +34,6 @@ options:
       - IP address part of the IP/port definition. If this parameter is not
         provided when creating a new monitor, then the default value will be
         '*'.
-      - If this value is an IP address, and the C(type) is C(tcp) (the default),
-        then a C(port) number must be specified.
   port:
     description:
       - Port address part of the IP/port definition. If this parameter is not
