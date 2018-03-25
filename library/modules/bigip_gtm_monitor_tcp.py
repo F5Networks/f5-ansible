a64974eee39d85c045c4812498db180d40fcbe97
Tim Rupp <caphrim007@gmail.com>
2018-03-24 22:15:00 -0700

    doc fixes

--- library/modules/bigip_gtm_monitor_tcp.py
+++ library/modules/bigip_gtm_monitor_tcp.py
@@ -41,15 +41,13 @@ options:
       - IP address part of the IP/port definition. If this parameter is not
         provided when creating a new monitor, then the default value will be
         '*'.
-      - If this value is an IP address, and the C(type) is C(tcp) (the default),
-        then a C(port) number must be specified.
+      - If this value is an IP address, then a C(port) number must be specified.
   port:
     description:
       - Port address part of the IP/port definition. If this parameter is not
         provided when creating a new monitor, then the default value will be
         '*'. Note that if specifying an IP address, a value between 1 and 65535
         must be specified
-      - This argument is not supported for TCP Echo types.
   interval:
     description:
       - The interval specifying how frequently the monitor instance of this
