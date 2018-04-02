8357313acfe88c010664cbd7f9b4aa3ce707bbf8
Tim Rupp <caphrim007@gmail.com>
2018-04-01 22:10:46 -0700

    import of unit tests for log destinations

--- test/unit/test_bigip_log_destination.py
+++ test/unit/test_bigip_log_destination.py
@@ -21,18 +21,26 @@ from ansible.compat.tests.mock import patch
 from ansible.module_utils.basic import AnsibleModule
 
 try:
-    from library.modules.bigip_log_destination import ApiParameters
-    from library.modules.bigip_log_destination import ModuleParameters
+    from library.modules.bigip_log_destination import V1ApiParameters
+    from library.modules.bigip_log_destination import V2ApiParameters
+    from library.modules.bigip_log_destination import V1ModuleParameters
+    from library.modules.bigip_log_destination import V2ModuleParameters
     from library.modules.bigip_log_destination import ModuleManager
+    from library.modules.bigip_log_destination import V1Manager
+    from library.modules.bigip_log_destination import V2Manager
     from library.modules.bigip_log_destination import ArgumentSpec
     from library.module_utils.network.f5.common import F5ModuleError
     from library.module_utils.network.f5.common import iControlUnexpectedHTTPError
     from test.unit.modules.utils import set_module_args
 except ImportError:
     try:
-        from ansible.modules.network.f5.bigip_log_destination import ApiParameters
-        from ansible.modules.network.f5.bigip_log_destination import ModuleParameters
+        from ansible.modules.network.f5.bigip_log_destination import V1ApiParameters
+        from ansible.modules.network.f5.bigip_log_destination import V2ApiParameters
+        from ansible.modules.network.f5.bigip_log_destination import V1ModuleParameters
+        from ansible.modules.network.f5.bigip_log_destination import V2ModuleParameters
         from ansible.modules.network.f5.bigip_log_destination import ModuleManager
+        from ansible.modules.network.f5.bigip_log_destination import V1Manager
+        from ansible.modules.network.f5.bigip_log_destination import V2Manager
         from ansible.modules.network.f5.bigip_log_destination import ArgumentSpec
         from ansible.module_utils.network.f5.common import F5ModuleError
         from ansible.module_utils.network.f5.common import iControlUnexpectedHTTPError
@@ -62,37 +70,60 @@ def load_fixture(name):
     return data
 
 
-class TestParameters(unittest.TestCase):
+class TestV1Parameters(unittest.TestCase):
     def test_module_parameters(self):
-        raise SkipTest('You must write your own module param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='m_of_n',
-        #     host='192.168.1.1',
-        #     port=8080
-        # )
-        #
-        # p = ModuleParameters(params=args)
-        # assert p.monitor == 'min 1 of'
-        # assert p.host == '192.168.1.1'
-        # assert p.port == 8080
+        args = dict(
+            name='foo',
+            syslog_settings=dict(
+                forward_to='pool1',
+                syslog_format='rfc5424'
+            ),
+            password='password',
+            server='localhost',
+            user='admin'
+        )
+        p = V1ModuleParameters(params=args)
+        assert p.name == 'foo'
+        assert p.forward_to == '/Common/pool1'
+        assert p.syslog_format == 'rfc5424'
 
     def test_api_parameters(self):
-        raise SkipTest('You must write your own API param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='and_list',
-        #     slowRampTime=200,
-        #     reselectTries=5,
-        #     serviceDownAction='drop'
-        # )
-        #
-        # p = ApiParameters(params=args)
-        # assert p.slow_ramp_time == 200
-        # assert p.reselect_tries == 5
-        # assert p.service_down_action == 'drop'
+        args = load_fixture('load_sys_log_config_destination_1.json')
+        p = V1ApiParameters(params=args)
+        assert p.name == 'foo'
+        assert p.syslog_format == 'rfc5424'
+        assert p.forward_to == '/Common/pool1'
 
 
-@patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
-       return_value=True)
-class TestManager(unittest.TestCase):
-    def test_create(self, *args):
-        raise SkipTest('You must write a creation test')
+class TestV1Manager(unittest.TestCase):
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
+    def test_create_policy(self, *args):
+        set_module_args(dict(
+            name="foo",
+            type='remote-syslog',
+            state='present',
+            password='password',
+            server='localhost',
+            user='admin'
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+
+        # Override methods in the specific type of manager
+        tm = V1Manager(module=module, params=module.params)
+        tm.exists = Mock(return_value=False)
+        tm.create_on_device = Mock(return_value=True)
+
+        # Override methods to force specific logic in the module to happen
+        mm = ModuleManager(module=module)
+        mm.get_manager = Mock(return_value=tm)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
