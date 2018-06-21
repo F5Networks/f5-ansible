509abad077830e79bc49b359c98fc81b80ab6d37
Tim Rupp <caphrim007@gmail.com>
2018-06-21 11:40:57 -0700

    Remove version from docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ import f5_sphinx_theme
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
 
-VERSION = "1.0.0"
+VERSION = ""
 
 # -- General configuration ------------------------------------------------
 
