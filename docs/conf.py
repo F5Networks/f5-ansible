7c254628e346081173578a088b667b604b76dd61
Tim Rupp <caphrim007@gmail.com>
2018-07-03 09:32:05 -0700

    adding last update format

--- docs/conf.py
+++ docs/conf.py
@@ -170,7 +170,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%Y-%m-%d %I:%M:%S'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
