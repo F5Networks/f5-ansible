a03df67bc219ebe6f82e82b0d8438cbf06682f4e
Tim Rupp <caphrim007@gmail.com>
2018-07-18 15:00:37 -0700

    Fixes: #801

--- library/modules/bigip_device_connectivity.py
+++ library/modules/bigip_device_connectivity.py
@@ -67,6 +67,16 @@ options:
         failover. When C(failover_multicast) is C(yes) and this option is not
         provided, a default of C(62960) will be used. This value must be between
         0 and 65535.
+  cluster_mirroring:
+    description:
+      - Specifies whether mirroring occurs within the same cluster or between
+        different clusters on a multi-bladed system.
+      - This parameter is only supported on platforms that have multiple blades,
+        such as Viprion hardware. It is not supported on VE.
+    choices:
+      - between-clusters
+      - within-cluster
+    version_added: 2.7
 notes:
   - This module is primarily used as a component of configuring HA pairs of
     BIG-IP devices.
@@ -134,6 +144,11 @@ multicast_port:
   returned: changed
   type: string
   sample: 1026
+cluster_mirroring:
+  description: The current cluster-mirroring setting.
+  returned: changed
+  type: string
+  sample: between-clusters
 '''
 
 from ansible.module_utils.basic import AnsibleModule
@@ -177,18 +192,35 @@ class Parameters(AnsibleF5Parameters):
         'managementIp': 'management_ip'
     }
     api_attributes = [
-        'configsyncIp', 'multicastInterface', 'multicastIp', 'multicastPort',
-        'mirrorIp', 'mirrorSecondaryIp', 'unicastAddress'
+        'configsyncIp',
+        'multicastInterface',
+        'multicastIp',
+        'multicastPort',
+        'mirrorIp',
+        'mirrorSecondaryIp',
+        'unicastAddress',
     ]
     returnables = [
-        'config_sync_ip', 'multicast_interface', 'multicast_address',
-        'multicast_port', 'mirror_primary_address', 'mirror_secondary_address',
-        'failover_multicast', 'unicast_failover'
+        'config_sync_ip',
+        'multicast_interface',
+        'multicast_address',
+        'multicast_port',
+        'mirror_primary_address',
+        'mirror_secondary_address',
+        'failover_multicast',
+        'unicast_failover',
+        'cluster_mirroring',
     ]
     updatables = [
-        'config_sync_ip', 'multicast_interface', 'multicast_address',
-        'multicast_port', 'mirror_primary_address', 'mirror_secondary_address',
-        'failover_multicast', 'unicast_failover'
+        'config_sync_ip',
+        'multicast_interface',
+        'multicast_address',
+        'multicast_port',
+        'mirror_primary_address',
+        'mirror_secondary_address',
+        'failover_multicast',
+        'unicast_failover',
+        'cluster_mirroring',
     ]
 
     @property
@@ -271,7 +303,13 @@ class Parameters(AnsibleF5Parameters):
 
 
 class ApiParameters(Parameters):
-    pass
+    @property
+    def cluster_mirroring(self):
+        if self._values['cluster_mirroring'] is None:
+            return None
+        if self._values['cluster_mirroring'] == 'between':
+            return 'between-clusters'
+        return 'within-cluster'
 
 
 class ModuleParameters(Parameters):
@@ -368,6 +406,14 @@ class UsableChanges(Changes):
         elif self._values['unicast_failover']:
             return self._values['unicast_failover']
         return "none"
+
+    @property
+    def cluster_mirroring(self):
+        if self._values['cluster_mirroring'] is None:
+            return None
+        elif self._values['cluster_mirroring'] == 'between-clusters':
+            return 'between'
+        return 'within'
 
 
 class Difference(object):
@@ -490,10 +536,14 @@ class ModuleManager(object):
         if self.module.check_mode:
             return True
         self.update_on_device()
+        if self.changes.cluster_mirroring:
+            self.update_cluster_mirroring_on_device()
         return True
 
     def update_on_device(self):
         params = self.changes.api_params()
+        if not params:
+            return
         collection = self.client.api.tm.cm.devices.get_collection()
         for resource in collection:
             if resource.selfDevice == 'true':
@@ -503,15 +553,24 @@ class ModuleManager(object):
             "The host device was not found."
         )
 
+    def update_cluster_mirroring_on_device(self):
+        resource = self.client.api.tm.sys.dbs.db.load(name='statemirror.clustermirroring')
+        resource.update(value=self.changes.cluster_mirroring)
+
     def read_current_from_device(self):
         collection = self.client.api.tm.cm.devices.get_collection()
         for resource in collection:
             if resource.selfDevice == 'true':
                 result = resource.attrs
+                result['cluster_mirroring'] = self.read_cluster_mirroring_from_device()
                 return ApiParameters(params=result)
         raise F5ModuleError(
             "The host device was not found."
         )
+
+    def read_cluster_mirroring_from_device(self):
+        resource = self.client.api.tm.sys.dbs.db.load(name='statemirror.clustermirroring')
+        return resource.value
 
 
 class ArgumentSpec(object):
@@ -531,7 +590,10 @@ class ArgumentSpec(object):
             ),
             mirror_primary_address=dict(),
             mirror_secondary_address=dict(),
-            config_sync_ip=dict()
+            config_sync_ip=dict(),
+            cluster_mirroring=dict(
+                choices=['within-cluster', 'between-clusters']
+            )
         )
         self.argument_spec = {}
         self.argument_spec.update(f5_argument_spec)
