60dda7b1f31593b648a14e0e395d1bd15cf24494
Tim Rupp <caphrim007@gmail.com>
2018-08-06 20:55:12 -0700

    add unit tests

--- test/unit/test_bigip_firewall_rule.py
+++ test/unit/test_bigip_firewall_rule.py
@@ -64,35 +64,90 @@ def load_fixture(name):
 
 class TestParameters(unittest.TestCase):
     def test_module_parameters(self):
-        raise SkipTest('You must write your own module param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='m_of_n',
-        #     host='192.168.1.1',
-        #     port=8080
-        # )
-        #
-        # p = ModuleParameters(params=args)
-        # assert p.monitor == 'min 1 of'
-        # assert p.host == '192.168.1.1'
-        # assert p.port == 8080
+        args = dict(
+            name='foo',
+            parent_policy='policy1',
+            protocol='tcp',
+            source=[
+                dict(address='1.2.3.4'),
+                dict(address='::1'),
+                dict(address_list='foo-list1'),
+                dict(address_range='1.1.1.1-2.2.2.2.'),
+                dict(vlan='vlan1'),
+                dict(country='US'),
+                dict(port='22'),
+                dict(port_list='port-list1'),
+                dict(port_range='80-443'),
+            ],
+            destination=[
+                dict(address='1.2.3.4'),
+                dict(address='::1'),
+                dict(address_list='foo-list1'),
+                dict(address_range='1.1.1.1-2.2.2.2.'),
+                dict(country='US'),
+                dict(port='22'),
+                dict(port_list='port-list1'),
+                dict(port_range='80-443'),
+            ],
+            irule='irule1',
+            action='accept',
+            logging=True,
+        )
 
-    def test_api_parameters(self):
-        raise SkipTest('You must write your own API param test. See examples, then remove this exception')
-        # args = dict(
-        #     monitor_type='and_list',
-        #     slowRampTime=200,
-        #     reselectTries=5,
-        #     serviceDownAction='drop'
-        # )
-        #
-        # p = ApiParameters(params=args)
-        # assert p.slow_ramp_time == 200
-        # assert p.reselect_tries == 5
-        # assert p.service_down_action == 'drop'
+        p = ModuleParameters(params=args)
+        assert p.irule == '/Common/irule1'
+        assert p.action == 'accept'
+        assert p.logging is True
 
 
 @patch('ansible.module_utils.f5_utils.AnsibleF5Client._get_mgmt_root',
        return_value=True)
 class TestManager(unittest.TestCase):
-    def test_create(self, *args):
-        raise SkipTest('You must write a creation test')
+
+    def setUp(self):
+        self.spec = ArgumentSpec()
+
+    def test_create_monitor(self, *args):
+        set_module_args(dict(
+            name='foo',
+            parent_policy='policy1',
+            protocol='tcp',
+            source=[
+                dict(address='1.2.3.4'),
+                dict(address='::1'),
+                dict(address_list='foo-list1'),
+                dict(address_range='1.1.1.1-2.2.2.2.'),
+                dict(vlan='vlan1'),
+                dict(country='US'),
+                dict(port='22'),
+                dict(port_list='port-list1'),
+                dict(port_range='80-443'),
+            ],
+            destination=[
+                dict(address='1.2.3.4'),
+                dict(address='::1'),
+                dict(address_list='foo-list1'),
+                dict(address_range='1.1.1.1-2.2.2.2.'),
+                dict(country='US'),
+                dict(port='22'),
+                dict(port_list='port-list1'),
+                dict(port_range='80-443'),
+            ],
+            irule='irule1',
+            action='accept',
+            logging='yes',
+        ))
+
+        module = AnsibleModule(
+            argument_spec=self.spec.argument_spec,
+            supports_check_mode=self.spec.supports_check_mode
+        )
+
+        # Override methods in the specific type of manager
+        mm = ModuleManager(module=module)
+        mm.exists = Mock(side_effect=[False, True])
+        mm.create_on_device = Mock(return_value=True)
+
+        results = mm.exec_module()
+
+        assert results['changed'] is True
