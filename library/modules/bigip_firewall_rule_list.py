6990decce6ffe7dc3e82347e136b93dba65f3455
Tim Rupp <caphrim007@gmail.com>
2018-08-18 23:14:14 -0700

    removing checks from the remove_from_device methods because the object being checked is an object, and dictionary fields are being checked

--- library/modules/bigip_firewall_rule_list.py
+++ library/modules/bigip_firewall_rule_list.py
@@ -374,11 +374,7 @@ class ModuleManager(object):
         resp = self.client.api.delete(uri)
         if resp.status == 200:
             return True
-        if 'code' in response and response['code'] == 400:
-            if 'message' in response:
-                raise F5ModuleError(response['message'])
-            else:
-                raise F5ModuleError(resp.content)
+        raise F5ModuleError(resp.content)
 
     def read_current_from_device(self):
         uri = "https://{0}:{1}/mgmt/tm/security/firewall/rule-list/{2}/?expandSubcollections=true".format(
