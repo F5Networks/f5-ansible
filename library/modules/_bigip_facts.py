c9589f95711a38f61d30eb599c31af304c23e690
Tim Rupp <caphrim007@gmail.com>
2018-09-13 10:29:37 -0700

    correct name

--- library/modules/_bigip_facts.py
+++ library/modules/_bigip_facts.py
@@ -15,7 +15,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 DOCUMENTATION = r'''
 ---
-module: _bigip_facts
+module: bigip_facts
 short_description: Collect facts from F5 BIG-IP devices
 description:
   - Collect facts from F5 BIG-IP devices via iControl SOAP API
