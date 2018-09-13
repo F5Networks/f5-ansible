59964ec35ef66abf1497406d063ee547196e11a7
Tim Rupp <caphrim007@gmail.com>
2018-09-13 08:56:53 -0700

    removing debugging

--- library/modules/bigip_software_install.py
+++ library/modules/bigip_software_install.py
@@ -437,12 +437,9 @@ class ModuleManager(object):
             except F5ModuleError:
                 pass
             time.sleep(5)
-        fh = open('/tmp/ansible.out', 'w')
-        import json
         while True:
             time.sleep(10)
             volume = self.read_volume_from_device()
-            fh.write(json.dumps(volume) + "\n")
             if volume is None or 'status' not in volume:
                 self.client.reconnect()
                 continue
