1504e3ec9f3d468299f95a34f981fdad6a7b0a01
Tim Rupp <caphrim007@gmail.com>
2018-10-10 14:58:02 -0700

    remove remaining sdk code from bigiq

--- library/module_utils/network/f5/bigiq.py
+++ library/module_utils/network/f5/bigiq.py
@@ -10,12 +10,6 @@ __metaclass__ = type
 import os
 import time
 
-try:
-    from f5.bigiq import ManagementRoot
-    from icontrol.exceptions import iControlUnexpectedHTTPError
-    HAS_F5SDK = True
-except ImportError:
-    HAS_F5SDK = False
 
 try:
     from library.module_utils.network.f5.common import F5BaseClient
@@ -27,39 +21,6 @@ except ImportError:
     from ansible.module_utils.network.f5.common import F5ModuleError
     from ansible.module_utils.network.f5.common import is_ansible_debug
     from ansible.module_utils.network.f5.icontrol import iControlRestSession
-
-
-class F5Client(F5BaseClient):
-    def __init__(self, *args, **kwargs):
-        super(F5Client, self).__init__(*args, **kwargs)
-        self.provider = self.merge_provider_params()
-
-    @property
-    def api(self):
-        exc = None
-        if self._client:
-            return self._client
-        for x in range(0, 10):
-            try:
-                result = ManagementRoot(
-                    self.provider['server'],
-                    self.provider['user'],
-                    self.provider['password'],
-                    port=self.provider['server_port'],
-                    verify=self.provider['validate_certs']
-                )
-                self._client = result
-                return self._client
-            except Exception as ex:
-                exc = ex
-                time.sleep(1)
-        error = 'Unable to connect to {0} on port {1}.'.format(
-            self.provider['server'], self.provider['server_port']
-        )
-
-        if exc is not None:
-            error += ' The reported error was "{0}".'.format(str(exc))
-        raise F5ModuleError(error)
 
 
 class F5RestClient(F5BaseClient):
