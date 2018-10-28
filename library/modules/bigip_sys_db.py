08dc0d51bbf99fceb3de434b17329592b0d43c85
Tim Rupp <caphrim007@gmail.com>
2018-10-27 20:00:17 -0700

    updating to certified metadata

--- library/modules/bigip_sys_db.py
+++ library/modules/bigip_sys_db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2016 F5 Networks Inc.
+# Copyright: (c) 2016, F5 Networks Inc.
 # GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
