27e87542e107bc8dbe878fef1ddfdf7f3504190d
Tim Rupp <caphrim007@gmail.com>
2018-10-30 10:51:23 -0700

    adding missing things during upstreaming to ansible core

--- library/modules/bigip_ipsec_policy.py
+++ library/modules/bigip_ipsec_policy.py
@@ -65,11 +65,18 @@ options:
     description:
       - Specifies the algorithm to use for IKE encryption.
     choices:
-      - null
+      - none
       - 3des
       - aes128
       - aes192
       - aes256
+      - aes-gmac256
+      - aes-gmac192
+      - aes-gmac128
+      - aes-gcm256
+      - aes-gcm192
+      - aes-gcm256
+      - aes-gcm128
   route_domain:
     description:
       - Specifies the route domain, when C(interface) is selected for the C(mode) setting.
@@ -95,6 +102,7 @@ options:
         compression algorithm.
     choices:
       - none
+      - "null"
       - deflate
   lifetime:
     description:
