59463542e38b3ef93fe9538fdab1a7cf30de7ec8
Tim Rupp <caphrim007@gmail.com>
2018-10-30 13:21:46 -0700

    remove f5-sdk from fast http profile facts

--- library/modules/bigip_device_facts.py
+++ library/modules/bigip_device_facts.py
@@ -6714,12 +6714,26 @@ class FastHttpProfilesFactManager(BaseManager):
         results = []
         collection = self.read_collection_from_device()
         for resource in collection:
-            params = FastHttpProfilesParameters(params=resource.attrs)
+            params = FastHttpProfilesParameters(params=resource)
             results.append(params)
         return results
 
     def read_collection_from_device(self):
-        result = self.client.api.tm.ltm.profile.fasthttps.get_collection()
+        uri = "https://{0}:{1}/mgmt/tm/ltm/profile/fasthttp".format(
+            self.client.provider['server'],
+            self.client.provider['server_port'],
+        )
+        resp = self.client.api.get(uri)
+        try:
+            response = resp.json()
+        except ValueError as ex:
+            raise F5ModuleError(str(ex))
+        if 'code' in response and response['code'] == 400:
+            if 'message' in response:
+                raise F5ModuleError(response['message'])
+            else:
+                raise F5ModuleError(resp.content)
+        result = response['items']
         return result
 
 
@@ -13202,7 +13216,8 @@ class ModuleManager(object):
                 client=F5RestClient
             ),
             'fasthttp-profiles': dict(
-                manager=FastHttpProfilesFactManager
+                manager=FastHttpProfilesFactManager,
+                client=F5RestClient
             ),
             'fastl4-profiles': dict(
                 manager=FastL4ProfilesFactManager,
