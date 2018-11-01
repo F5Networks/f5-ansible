09cae6fdb7a652b92964fe297dcf4c0e7f6c1911
Wojciech Wypior <w.wypior@f5.com>
2018-11-01 11:50:00 +0000

    moving parseStats to urls.py removing SDK from module

--- library/module_utils/network/f5/urls.py
+++ library/module_utils/network/f5/urls.py
@@ -58,3 +58,65 @@ def build_service_uri(base_uri, partition, name):
     """
     name = name.replace('/', '~')
     return '%s~%s~%s.app~%s' % (base_uri, partition, name, name)
+
+
+def parseStats(entry):
+    if 'description' in entry:
+        return entry['description']
+    elif 'value' in entry:
+        return entry['value']
+    elif 'entries' in entry or 'nestedStats' in entry and 'entries' in entry['nestedStats']:
+        if 'entries' in entry:
+            entries = entry['entries']
+        else:
+            entries = entry['nestedStats']['entries']
+        result = None
+
+        for name in entries:
+            entry = entries[name]
+            if 'https://localhost' in name:
+                name = name.split('/')
+                name = name[-1]
+                if result and isinstance(result, list):
+                    result.append(parseStats(entry))
+                elif result and isinstance(result, dict):
+                    result[name] = parseStats(entry)
+                else:
+                    try:
+                        int(name)
+                        result = list()
+                        result.append(parseStats(entry))
+                    except ValueError:
+                        result = dict()
+                        result[name] = parseStats(entry)
+            else:
+                if '.' in name:
+                    names = name.split('.')
+                    key = names[0]
+                    value = names[1]
+                    if result is None:
+                        # result can be None if this branch is reached first
+                        #
+                        # For example, the mgmt/tm/net/trunk/NAME/stats API
+                        # returns counters.bitsIn before anything else.
+                        result = dict()
+                        result[key] = dict()
+                    elif key not in result:
+                        result[key] = dict()
+                    elif result[key] is None:
+                        result[key] = dict()
+                    result[key][value] = parseStats(entry)
+                else:
+                    if result and isinstance(result, list):
+                        result.append(parseStats(entry))
+                    elif result and isinstance(result, dict):
+                        result[name] = parseStats(entry)
+                    else:
+                        try:
+                            int(name)
+                            result = list()
+                            result.append(parseStats(entry))
+                        except ValueError:
+                            result = dict()
+                            result[name] = parseStats(entry)
+        return result
