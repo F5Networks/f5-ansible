8a55a52154f16fee0d4e133ae59176246a601854
Tim Rupp <caphrim007@gmail.com>
2018-11-16 20:52:53 -0800

    remove f5-sdk references

--- test/units/modules/network/f5/test_bigip_device_facts.py
+++ test/units/modules/network/f5/test_bigip_device_facts.py
@@ -34,24 +34,22 @@ try:
 
     from test.units.modules.utils import set_module_args
 except ImportError:
-    try:
-        from ansible.modules.network.f5.bigip_device_pool import Parameters
-        from ansible.modules.network.f5.bigip_device_pool import VirtualAddressesFactManager
-        from ansible.modules.network.f5.bigip_device_pool import VirtualAddressesParameters
-        from ansible.modules.network.f5.bigip_device_pool import ArgumentSpec
-        from ansible.modules.network.f5.bigip_device_pool import ModuleManager
+    from ansible.modules.network.f5.bigip_device_pool import Parameters
+    from ansible.modules.network.f5.bigip_device_pool import VirtualAddressesFactManager
+    from ansible.modules.network.f5.bigip_device_pool import VirtualAddressesParameters
+    from ansible.modules.network.f5.bigip_device_pool import ArgumentSpec
+    from ansible.modules.network.f5.bigip_device_pool import ModuleManager
 
-        from f5.bigip.tm.gtm.pool import A
-        from f5.utils.responses.handlers import Stats
+    from f5.bigip.tm.gtm.pool import A
+    from f5.utils.responses.handlers import Stats
 
-        # Ansible 2.8 imports
-        from units.compat import unittest
-        from units.compat.mock import Mock
-        from units.compat.mock import patch
+    # Ansible 2.8 imports
+    from units.compat import unittest
+    from units.compat.mock import Mock
+    from units.compat.mock import patch
 
-        from units.modules.utils import set_module_args
-    except ImportError:
-        raise SkipTest("F5 Ansible modules require the f5-sdk Python library")
+    from units.modules.utils import set_module_args
+
 
 fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
 fixture_data = {}
