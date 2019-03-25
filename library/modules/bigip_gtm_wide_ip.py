a999a52adb75f32569c465e8e6e34d6ee5b54790
Wojciech Wypior <w.wypior@f5.com>
2019-03-25 17:00:27 +0100

    style fixes

--- library/modules/bigip_gtm_wide_ip.py
+++ library/modules/bigip_gtm_wide_ip.py
@@ -668,8 +668,8 @@ class BaseManager(object):
             dict(
                 msg='The support for this TMOS version is deprecated.',
                 version='2.8'
-                )
             )
+        )
 
     def present(self):
         if self.exists():
