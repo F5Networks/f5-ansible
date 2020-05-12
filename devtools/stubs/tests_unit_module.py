9859d08591a6dcda94568a80c045485506dcdb04
Wojciech Wypior <w.wypior@f5.com>
2020-05-12 05:20:19 +0200

    fixes #1485

--- devtools/stubs/tests_unit_module.py
+++ devtools/stubs/tests_unit_module.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright: (c) 2019, F5 Networks Inc.
+# Copyright: (c) 2020, F5 Networks Inc.
 # GNU General Public License v3.0 (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import (absolute_import, division, print_function)
