01e24a2cbd4d539803688be091e400666199f24c
marcinwito <46540931+marcinwito@users.noreply.github.com>
2020-10-12 21:46:02 +0200

    add request options to reset type

--- ansible_collections/f5networks/f5_modules/plugins/modules/bigip_policy_rule.py
+++ ansible_collections/f5networks/f5_modules/plugins/modules/bigip_policy_rule.py
@@ -693,6 +693,11 @@ class ModuleParameters(Parameters):
                 connection=True,
                 shutdown=True
             ))
+        elif 'request' in item['event']:
+            action.update(dict(
+                connection=True,
+                shutdown=True
+            ))            
 
     def _handle_persist_action(self, action, item):
         """Handle the nuances of the persist type
