4c153b881217b6c31f3116749784b525ff4be411
Joe Jordan <j.jordan@f5.com>
2023-03-20 17:25:59 -0700

    updating boolean values from yes/no to true/false

--- ansible_collections/f5networks/f5_modules/plugins/modules/bigiq_application_fasthttp.py
+++ ansible_collections/f5networks/f5_modules/plugins/modules/bigiq_application_fasthttp.py
@@ -86,7 +86,7 @@ options:
       - This parameter is only relevant when specifying a C(service_environment) which
         is a BIG-IP; not an SSG.
     type: bool
-    default: no
+    default: false
   state:
     description:
       - The state of the resource on the system.
@@ -101,7 +101,7 @@ options:
     description:
       - If the module should wait for the application to be created, deleted, or updated.
     type: bool
-    default: yes
+    default: true
 extends_documentation_fragment: f5networks.f5_modules.f5
 notes:
   - This module does not support updating of your application (whether deployed or not).
