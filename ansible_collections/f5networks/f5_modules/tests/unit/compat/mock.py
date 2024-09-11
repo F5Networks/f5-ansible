d7a98a2fc0f493d10a6cb263781bdc03e0965da3
Rohit Upadhyay <r.upadhyay@f5.com>
2024-09-11 19:00:13 +0530

    Fixed a few sanity test errors

--- ansible_collections/f5networks/f5_modules/tests/unit/compat/mock.py
+++ ansible_collections/f5networks/f5_modules/tests/unit/compat/mock.py
@@ -64,8 +64,7 @@ if sys.version_info >= (3,) and sys.version_info < (3, 4, 4):
             # newline that our naive format() added
             data_as_list[-1] = data_as_list[-1][:-1]
 
-        for line in data_as_list:
-            yield line
+        yield from data_as_list
 
     def mock_open(mock=None, read_data=''):
         """
@@ -93,8 +92,7 @@ if sys.version_info >= (3,) and sys.version_info < (3, 4, 4):
             if handle.readline.return_value is not None:
                 while True:
                     yield handle.readline.return_value
-            for line in _data:
-                yield line
+            yield from _data
 
         global file_spec
         if file_spec is None:
