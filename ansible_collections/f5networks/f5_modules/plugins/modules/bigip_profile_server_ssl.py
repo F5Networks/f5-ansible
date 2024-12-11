a5db6e37b498fb221ca60a9ae50e21642a03d3a6
ramaniprateek <p.ramani@f5.com>
2024-12-11 13:04:36 +0530

    Fixed Issue 2435 - bigip_profile_server_ssl fails to create server SSL profile if SSL key is passphrase protected

--- ansible_collections/f5networks/f5_modules/plugins/modules/bigip_profile_server_ssl.py
+++ ansible_collections/f5networks/f5_modules/plugins/modules/bigip_profile_server_ssl.py
@@ -269,6 +269,7 @@ class Parameters(AnsibleF5Parameters):
         'caFile',
         'authenticateName',
         'tmOptions',
+        'passphrase'
     ]
 
     returnables = [
@@ -440,6 +441,13 @@ class ModuleParameters(Parameters):
             return []
         return options
 
+    @property
+    def passphrase(self):
+        if self._values['passphrase'] is None:
+            return None
+        if self._values['passphrase'] in ['', 'none']:
+            return ''
+        return self._values['passphrase']
 
 class Changes(Parameters):
     def to_return(self):
@@ -699,6 +707,7 @@ class ModuleManager(object):
             self.client.provider['server'],
             self.client.provider['server_port']
         )
+        params['passphrase'] = self.want.passphrase
         resp = self.client.api.post(uri, json=params)
         try:
             response = resp.json()
