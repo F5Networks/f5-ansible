f291d391426203b28c2a2b0e6639d1c1ed5b2a2f
mpanchal <m.panchal@f5.com>
2025-04-22 20:50:19 +0530

    Environment variables can be configured through playbook and provider and also bash envs as well

---
+++ ansible_collections/f5networks/f5_modules/plugins/module_utils/provider_fallback.py
@@ -0,0 +1,22 @@
+import os
+
+def smart_fallback(module, varnames):
+    """
+    Compatible with Ansible's load_provider() fallback logic.
+    1. Checks Ansible task-level environment (via `environment:`)
+    2. Falls back to process env (os.environ)
+    """
+    try:
+        if module and hasattr(module._task, 'environment'):
+            env = module._task.environment
+            for var in varnames:
+                if var in env:
+                    return env[var]
+    except Exception:
+        pass
+
+    for var in varnames:
+        if var in os.environ:
+            return os.environ[var]
+
+    return None
